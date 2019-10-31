bcad1a77af65bcb65b0b10fedf0f41e00537d97a
Elena Secara <elena.secara@stockport.gov.uk>
2019-10-31 17:03:42 +0000

    feature(Textbox): added textbox unit tests

---
+++ form-builder-tests-ui/Features/Textbox.feature
@@ -0,0 +1,18 @@
+﻿@textbox
+Feature: Textbox
+	In order to fill in my details I have to navigate to Page1
+
+
+Scenario: Renders HTML tags on the page
+	Given I navigate to "/Textbox/page1"
+	Then I should see the header
+	And I should see the "firstName" input
+	And I should see the "lastName" input
+	And I should see the "nextStep" button
+
+
+Scenario: User fills in data and clicks next
+	Given I navigate to "/Textbox/page1"
+	Then I fill in page1
+	Then I press the next step button
+	Then I should see the "otherNames" input
---
+++ form-builder-tests-ui/StepDefinitions/TextboxSteps.cs
@@ -0,0 +1,21 @@
+﻿using TechTalk.SpecFlow;
+
+namespace form_builder_tests_ui.StepDefinitions
+{
+    [Binding, Scope(Tag = "textbox")]
+    public class Textbox : UiTestBase
+    {
+        [Then(@"I fill in page1")]
+        public void ThenIFillInPage1()
+        {
+            BrowserSession.FillIn("firstName").With("test");
+            BrowserSession.FillIn("lastName").With("test");          
+        }
+
+        [Then(@"I press the next step button")]
+        public void ThenIPressTheNextStepButton()
+        {
+            BrowserSession.ClickButton("nextStep");
+        }
+    }
+}
