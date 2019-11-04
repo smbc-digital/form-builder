bce79841813bcd8103d60ad4b195a574fc0c2bf8
Elena Secara <elena.secara@stockport.gov.uk>
2019-11-04 16:41:27 +0000

    chore(Session): Removed session config for now

--- src/Startup.cs
+++ src/Startup.cs
@@ -27,7 +27,7 @@ namespace form_builder
         {
             services
                 .ConfigureCookiePolicy()
-                .AddCacheProvider(HostingEnvironment.IsEnvironment("local"))
+                .AddCacheProvider(Configuration.GetSection("RedisConfiguration")["RedisUrl"])
                 .AddValidators()
                 .AddSchemaProvider(HostingEnvironment)
                 .AddAmazonS3Client(Configuration.GetSection("AmazonS3Configuration")["AccessKey"], Configuration.GetSection("AmazonS3Configuration")["SecretKey"])
@@ -52,8 +52,6 @@ namespace form_builder
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            app.UseSession();
 
             app.UseHttpsRedirection();
             app.UseMvc(routes =>
