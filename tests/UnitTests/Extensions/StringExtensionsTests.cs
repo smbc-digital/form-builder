eb39eb76bbe1f7e96a6e61e9be966d2b216077e8
stephen.lally <stephen.lally.smbc@gmail.com>
2019-11-21 09:08:35 +0000

    tests(StringExtension): Added test for string extension

---
+++ tests/UnitTests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using form_builder.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace form_builder_tests.UnitTests.Extensions
+{
+    public class StringExtensionsTests
+    {
+
+        [Theory]
+        [InlineData("ui-test", "Int")]
+        [InlineData("local", "Int")]
+        [InlineData("int", "Int")]
+        [InlineData("qa", "QA")]
+        [InlineData("stage", "Staging")]
+        [InlineData("prod", "Prod")]
+        public void ToS3EnvPrefix_ShouldReturnCorrectValueForEnv(string env, string expectedResult)
+        {
+            var testString = env;
+
+            Assert.Equal(expectedResult, testString.ToS3EnvPrefix());
+        }
+
+        [Fact]
+        public void ToS3EnvPrefix_ShouldThrowException_WhenUnknownEnv()
+        {
+            var testString = "unkown-env";
+
+            Assert.Throws<Exception>(() => testString.ToS3EnvPrefix());
+        }
+    }
+}
