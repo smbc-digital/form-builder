8077f06cb57eb75bddebd0c61e68be3f30839e2e
stephen.lally <stephen.lally.smbc@gmail.com>
2019-11-21 09:08:12 +0000

    chore(Project): Exclude Service collection from code coverage

--- src/Extensions/ServiceCollectionExtensions.cs
+++ src/Extensions/ServiceCollectionExtensions.cs
@@ -13,9 +13,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
 
 namespace form_builder.Extensions
 {
+    [ExcludeFromCodeCoverage]
     public static class ServiceCollectionExtensions
     {
         public static IServiceCollection AddValidators(this IServiceCollection services)
