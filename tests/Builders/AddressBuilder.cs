9aff060b264b66682c74d015a6ce13ee50401257
stephen.lally <stephen.lally.smbc@gmail.com>
2019-12-04 09:55:58 +0000

    fix(Address/Street): fixed unit tests for adress and street

---
+++ tests/Builders/AddressBuilder.cs
@@ -0,0 +1,30 @@
+﻿using form_builder.Models;
+using form_builder.Models.Elements;
+
+namespace form_builder_tests.Builders
+{
+    public class AddressBuilder
+    {
+        private Property _property = new Property();
+
+        public Address Build()
+        {
+            return new Address
+            {
+                Properties = _property,
+            };
+        }
+
+        public AddressBuilder WithPropertyText(string propertyText)
+        {
+            _property.Text = propertyText;
+            return this;
+        }
+
+        public AddressBuilder WithQuestionId(string questionId)
+        {
+            _property.QuestionId = questionId;
+            return this;
+        }
+    }
+}
---
+++ tests/UnitTests/Models/Elements/StreetTests.cs
@@ -0,0 +1,76 @@
+﻿using form_builder.Helpers;
+using form_builder.Helpers.ElementHelpers;
+using form_builder.Models;
+using form_builder.Models.Elements;
+using form_builder.ViewModels;
+using form_builder_tests.Builders;
+using Microsoft.AspNetCore.Hosting;
+using Moq;
+using StockportGovUK.NetStandard.Models.Addresses;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace form_builder_tests.UnitTests.Models.Elements
+{
+    public class StreetTests
+    {
+        private readonly Mock<IViewRender> _mockIViewRender = new Mock<IViewRender>();
+        private readonly Mock<IElementHelper> _mockElementHelper = new Mock<IElementHelper>();
+        private readonly Mock<IHostingEnvironment> _mockHostingEnv = new Mock<IHostingEnvironment>();
+
+        public StreetTests()
+        {
+            _mockHostingEnv.Setup(_ => _.EnvironmentName).Returns("local");
+        }
+
+        [Fact]
+        public async Task Street_ShouldCallViewRenderWithCorrectPartial_WhenStreetSearch()
+        {
+            var element = new form_builder.Models.Elements.Street { Properties = new Property { Text = "text", QuestionId = "street" } };
+
+            var page = new PageBuilder()
+                .WithElement(element)
+                .Build();
+
+            var viewModel = new Dictionary<string, string>();
+            viewModel.Add("StreetStatus", "Select");
+
+            var schema = new FormSchemaBuilder()
+                .WithName("Street name")
+                .Build();
+
+            //Act
+            var result = await element.RenderAsync(_mockIViewRender.Object, _mockElementHelper.Object, "", new List<AddressSearchResult>(), new List<StockportGovUK.NetStandard.Models.Models.Verint.Street>(), viewModel, page, schema, _mockHostingEnv.Object);
+
+
+            _mockIViewRender.Verify(_ => _.RenderAsync(It.Is<string>(x => x == "StreetSelect"), It.IsAny<Tuple<ElementViewModel, List<StockportGovUK.NetStandard.Models.Models.Verint.Street>>>(), It.IsAny<Dictionary<string, object>>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task Street_ShouldCallViewRenderWithCorrectPartial_WhenStreetSelect()
+        {
+            //Arrange
+            var element = new form_builder.Models.Elements.Street { Properties = new Property { Text = "text", QuestionId = "street", StreetProvider = "test" } };
+
+            var page = new PageBuilder()
+                .WithElement(element)
+                .Build();
+
+            var viewModel = new Dictionary<string, string>();
+            viewModel.Add("StreetStatus", "Search");
+
+            var schema = new FormSchemaBuilder()
+                .WithName("Street name")
+                .Build();
+
+            //Act
+            var result = await element.RenderAsync(_mockIViewRender.Object, _mockElementHelper.Object, "", new List<AddressSearchResult>(), new List<StockportGovUK.NetStandard.Models.Models.Verint.Street>(), viewModel, page, schema, _mockHostingEnv.Object);
+
+            //Assert
+            _mockIViewRender.Verify(_ => _.RenderAsync(It.Is<string>(x => x == "StreetSearch"),It.IsAny<Element>(), It.IsAny<Dictionary<string, object>>()), Times.Once);
+        }
+
+    }
+}
