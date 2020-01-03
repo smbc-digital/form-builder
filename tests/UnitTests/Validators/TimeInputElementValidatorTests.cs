8e9fc8928ffe935e5e2f435d8e084328769742a6
Simon Estill <simon.estill@stockport.gov.uk>
2020-01-03 11:05:44 +0000

    Task added validator tests

---
+++ tests/UnitTests/Validators/TimeInputElementValidatorTests.cs
@@ -0,0 +1,63 @@
+﻿using form_builder.Enum;
+using form_builder.Validators;
+using form_builder_tests.Builders;
+using System.Collections.Generic;
+using Xunit;
+
+namespace form_builder_tests.UnitTests.Validators
+{
+    public class TimeInputElementValidatorTests
+    {
+        private readonly TimeInputValidator _dateInputElementValidator = new TimeInputValidator();
+
+        [Fact]
+        public void Validate_ShouldCheckTheElementTypeIsNotDateInput()
+        {
+            //Arrange
+            var element = new ElementBuilder()
+                .WithType(EElementType.Textbox)
+                .Build();
+
+            //Assert
+            var result = _dateInputElementValidator.Validate(element, null);
+            Assert.True(result.IsValid);
+        }
+        [Fact]
+        public void Validate_ShouldShowValidationMessageWhenFieldsAreEmpty()
+        {
+            //Arrange
+            var element = new ElementBuilder()
+                .WithType(EElementType.TimeInput)
+                .WithQuestionId("test-time")
+                .WithLabel("Time")
+                .Build();
+
+            var viewModel = new Dictionary<string, string>();
+
+            //Assert
+            var result = _dateInputElementValidator.Validate(element, viewModel);
+            Assert.False(result.IsValid);
+            Assert.Equal("Check the time and try again", result.Message);
+        }
+
+        [Fact]
+       public void Validate_ShouldCheckTimeIsNotValid()
+        {
+            //Arrange
+            var element = new ElementBuilder()
+                .WithType(EElementType.TimeInput)
+                .WithQuestionId("test-time")
+                .Build();
+
+            var viewModel = new Dictionary<string, string>();
+            viewModel.Add("test-time-hours", "aa");
+            viewModel.Add("test-time-minutes", "aa");
+            viewModel.Add("test-time-ampm", "aaaa");
+
+            //Assert
+            var result = _dateInputElementValidator.Validate(element, viewModel);
+            Assert.False(result.IsValid);
+            Assert.Equal("Check the time and try again", result.Message);
+        }
+    }
+}
