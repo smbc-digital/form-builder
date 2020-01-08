b0ee164628051f7f255a85e262b9234690ccc7fc
Simon Estill <simon.estill@stockport.gov.uk>
2020-01-08 18:28:09 +0000

    Added  numeric

---
+++ src/Models/Properties/DatePickerProperties.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace form_builder.Models.Properties
+{
+    public class DatePickerProperties : BaseProperty
+    {
+        string max { get; set; } = string.Empty;
+        string min { get; set; } = string.Empty;
+    }
+}
