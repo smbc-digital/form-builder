ecb92edb972aba936186f93af3e94ba2ad69ed0a
Ruha Bashir <ruha.bashir@stockport.gov.uk>
2020-01-10 10:20:22 +0000

    feat(ButtonBehaviour): put javascript in a separate file

---
+++ src/wwwroot/assets/javascript/buttonBehaviour.js
@@ -0,0 +1,9 @@
+﻿function disableBtn(buttonId) {
+    var disabledButton = document.getElementById(buttonId);
+    if (!disabledButton.className.includes("is-loading")) {
+        disabledButton.className += " button-loading is-loading";
+        return;
+    }
+    disabledButton.setAttribute("type", "button");
+    return;
+}
