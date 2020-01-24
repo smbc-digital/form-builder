b271595c351b7388b402299df6a14908ca2378aa
stephen.lally <stephen.lally.smbc@gmail.com>
2020-01-24 13:44:46 +0000

    chore(PayProvider): Added env return prefix for non local

--- src/Providers/PaymentProvider/CivicaPayProvider.cs
+++ src/Providers/PaymentProvider/CivicaPayProvider.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
 using form_builder.Configuration;
+using form_builder.Extensions;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
 using StockportGovUK.NetStandard.Gateways.Civica.Pay;
@@ -15,12 +17,14 @@ namespace form_builder.Providers.PaymentProvider
         private readonly ICivicaPayGateway _civicaPayGateway;
         private readonly CivicaPaymentConfiguration _paymentConfig;
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IHostingEnvironment _environment;
 
-        public CivicaPayProvider(ICivicaPayGateway civicaPayGateway, IOptions<CivicaPaymentConfiguration> paymentConfiguration, IHttpContextAccessor httpContextAccessor)
+        public CivicaPayProvider(ICivicaPayGateway civicaPayGateway, IOptions<CivicaPaymentConfiguration> paymentConfiguration, IHttpContextAccessor httpContextAccessor, IHostingEnvironment environment)
         {
             _civicaPayGateway = civicaPayGateway;
             _httpContextAccessor = httpContextAccessor;
             _paymentConfig = paymentConfiguration.Value;
+            _environment = environment;
         }
         public async Task<string> GeneratePaymentUrl(string form, string path, string reference, string sessionGuid, PaymentInformation paymentInformation)
         {
@@ -29,7 +33,8 @@ namespace form_builder.Providers.PaymentProvider
                 CallingAppIdentifier = "Basket",
                 CustomerID = _paymentConfig.CustomerId,
                 ApiPassword = _paymentConfig.ApiPassword,
-                ReturnURL = $"{_httpContextAccessor.HttpContext.Request.Scheme}://{_httpContextAccessor.HttpContext.Request.Host}/{form}/{path}/payment-response",
+                //$"{environment.EnvironmentName.ToReturnUrlPrefix()}/{formSchema.BaseURL}/{page.PageSlug}";
+                ReturnURL = $"{_httpContextAccessor.HttpContext.Request.Scheme}://{_httpContextAccessor.HttpContext.Request.Host}{_environment.EnvironmentName.ToReturnUrlPrefix()}/{form}/{path}/payment-response",
                 NotifyURL = string.Empty,
                 CallingAppTranReference = reference,
                 PaymentItems = new List<PaymentItem>
