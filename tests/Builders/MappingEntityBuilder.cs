0b2ad710de91ff7cf26faf8844e36b359399bdfa
Ruha Bashir <ruha.bashir@stockport.gov.uk>
2020-02-17 14:35:22 +0000

    refactor(PayServiceTests): add mapping entity builder and refactor code in constructor

---
+++ tests/Builders/MappingEntityBuilder.cs
@@ -0,0 +1,32 @@
+﻿using form_builder.Models;
+using form_builder.Services.MappingService.Entities;
+
+namespace form_builder_tests.Builders
+{
+    public class MappingEntityBuilder
+    {
+        private FormSchema _baseForm = new FormSchema();
+        private FormAnswers _formAnswers = new FormAnswers();
+
+        public MappingEntity Build()
+        {
+            return new MappingEntity
+            {
+                BaseForm = _baseForm,
+                FormAnswers = _formAnswers
+            };
+        }
+
+        public MappingEntityBuilder WithBaseForm(FormSchema baseForm)
+        {
+            _baseForm = baseForm;
+            return this;
+        }
+
+        public MappingEntityBuilder WithFormAnswers(FormAnswers formAnswers)
+        {
+            _formAnswers = formAnswers;
+            return this;
+        }
+    }
+}
