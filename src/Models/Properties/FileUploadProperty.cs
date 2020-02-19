a22af78952381adc0b6a76c43043672e0bebec2d
Rowan Dennigan <rowan.dennigan@stockport.gov.uk>
2020-02-19 15:26:21 +0000

    fixed issue with default mime types

--- src/Models/Properties/FileUploadProperty.cs
+++ src/Models/Properties/FileUploadProperty.cs
@@ -7,7 +7,22 @@ namespace form_builder.Models.Properties
 {
     public partial class BaseProperty
     {
-        public List<String> AllowedFileTypes { get; set; }
+        private List<string> _allowedFileTypes;
+
+        public List<String> AllowedFileTypes
+        {
+            get
+            {
+                if (_allowedFileTypes == null)
+                {
+                    _allowedFileTypes = new List<string>();
+                    _allowedFileTypes.Add("All Types");
+                }
+                return _allowedFileTypes;
+            }
+            set => _allowedFileTypes = value;
+        }
+
         public int MaxFileSize { get; set; }
     }
 }
