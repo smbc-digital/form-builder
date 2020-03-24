5443967632b7aeea8a4b10f47000fba1f969a742
Rowan Dennigan <rowan.dennigan@stockport.gov.uk>
2020-03-24 14:10:05 +0000

    changed validation script to ensure all errors are fixed before enabling goto

--- src/wwwroot/assets/javascript/fileSizeCheck.js
+++ src/wwwroot/assets/javascript/fileSizeCheck.js
@@ -1,4 +1,21 @@
-﻿function ValidateSize(file) {
+﻿document['getElementsByRegex'] = function (pattern) {
+    var arrElements = [];   // to accumulate matching elements
+    var re = new RegExp(pattern);   // the regex to match with
+
+    function findRecursively(aNode) { // recursive function to traverse DOM
+        if (!aNode)
+            return;
+        if (aNode.id !== undefined && aNode.id.search(re) != -1)
+            arrElements.push(aNode);  // FOUND ONE!
+        for (var idx in aNode.childNodes) // search children...
+            findRecursively(aNode.childNodes[idx]);
+    };
+
+    findRecursively(document); // initiate recursive matching
+    return arrElements; // return matching elements
+};
+
+function ValidateSize(file) {
     var FileSize = file.files[0].size / 1024 / 1024; // in MB
     var sizeValidation = document.getElementById(`${file.id}-fileSizeError`);
     var next = document.getElementById("submit");
@@ -13,7 +30,15 @@
         input.setAttribute("aria-describedby", `${file.id}-fileSizeError`);
     } else {
         sizeValidation.style.display = "none";
-        next.disabled = false;
         input.removeAttribute("aria-describedby");
+        var fileSizeErrors = document.getElementsByRegex('-fileSizeError*');
+        var errorCount = 0;
+        for (var input = 0; input < fileSizeErrors.length; input++) {
+            if (fileSizeErrors[input].style.display === 'block') {
+                errorCount++;
+            }
+        }
+        if (errorCount <= 0)
+            next.disabled = false;
     }
 }
