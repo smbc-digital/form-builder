550cc60cef9ccd4de50543731df9feb3ef7b401b
Rowan Dennigan <rowan.dennigan@stockport.gov.uk>
2020-03-26 14:19:37 +0000

    Fixed file names

--- src/Mappers/ElementMapper.cs
+++ src/Mappers/ElementMapper.cs
@@ -81,8 +81,8 @@ namespace form_builder.Mappers
                 };
 
                 model.Content = fileData;
-                model.TrustedOriginalFileName = uploadModel.TrustedOriginalFileName;
-                model.UntrustedOriginalFileName = uploadModel.UntrustedOriginalFileName;
+                model.TrustedOriginalFileName = uploadModel.UntrustedOriginalFileName;
+                model.UntrustedOriginalFileName = uploadModel.TrustedOriginalFileName;
                 return model;
             }
             return null;
