d01b89760516b9d67ee1b7c12c79bd86c9ebb5eb
Jon Hadley <hadley.jk@googlemail.com>
2020-03-30 15:51:24 +0100

    feature(button) Update buttons styles and add disbale property

--- src/Models/Properties/AddressProperty.cs
+++ src/Models/Properties/AddressProperty.cs
@@ -14,5 +14,6 @@
         public bool? Postcode { get; set; }
         public bool? StockportPostcode { get; set; }
         public bool DisplayNoResultsIAG { get; set; } = false;
+        public string Disabled {get; set; } = string.Empty;
     }
 }
