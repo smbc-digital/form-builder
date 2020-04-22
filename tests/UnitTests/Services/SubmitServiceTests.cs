fc96b26955f40a60f7fa34dea31cb59187bc7f8b
Jon Hadley <hadley.jk@googlemail.com>
2020-04-22 09:22:47 +0100

    fix - broken test after merge

--- tests/UnitTests/Services/SubmitServiceTests.cs
+++ tests/UnitTests/Services/SubmitServiceTests.cs
@@ -86,10 +86,10 @@ namespace form_builder_tests.UnitTests.Services
             _sessionHelper.Setup(_ => _.GetSessionGuid()).Returns("123454");
 
             var element = new ElementBuilder()
-                 .WithType(EElementType.H1)
-                 .WithQuestionId("test-id")
-                 .WithPropertyText("test-text")
-                 .Build();
+                .WithType(EElementType.H1)
+                .WithQuestionId("test-id")
+                .WithPropertyText("test-text")
+                .Build();
 
             var behaviour = new BehaviourBuilder()
                 .WithBehaviourType(EBehaviourType.SubmitForm)
@@ -110,7 +110,7 @@ namespace form_builder_tests.UnitTests.Services
             var result = await Assert.ThrowsAsync<NullReferenceException>(() => _service.ProcessSubmission(new MappingEntity { BaseForm = schema, FormAnswers = new FormAnswers { Path = "page-one" } }, "form", ""));
 
             // Assert
-            Assert.Equal("HomeController, Submit: No postUrl supplied for submit form", result.Message);
+            Assert.Equal("Page model, Submit: No postUrl supplied for submit form", result.Message);
             _mockGateway.Verify(_ => _.PostAsync(It.IsAny<string>(), It.IsAny<object>()), Times.Never);
         }
 
