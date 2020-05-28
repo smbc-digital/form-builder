1c75164bdf24c55f190b217715b5b0f1b53004f5
Lydia Gray <lydia.gray@stockport.gov.uk>
2020-05-28 16:11:37 +0100

    fix(UI test): fix broken button UI test

---
+++ form-builder-tests-ui/StepDefinitions/ButtonSteps.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace form_builder_tests_ui.StepDefinitions
+{
+    [Binding, Scope(Tag = "button")]
+    public class ButtonSteps : UiTestBase
+    {
+        [Then(@"I should see an element with ID of ""(.*)""")]
+        public void ThenIShouldSeeAElement(string elementType)
+        {
+            var webDriver = BrowserSession.Native as IWebDriver;
+            Assert.True(webDriver.FindElement(By.Id($"{elementType}")).Displayed);
+        }
+
+        [Then(@"I wait one second")]
+        public void ThenIWaitOneSecond()
+        {
+            Thread.Sleep(1000);
+        }
+    }
+}
