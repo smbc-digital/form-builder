5a86bf6e900c31c99cce27dd2cbb27c4c19b1dcd
Chilejon <jon.chiles@stockport.gov.uk>
2020-06-01 16:20:17 +0100

    Fix(): textareas sizes now right in terms of size based of max length (#33)
    * Fix(): textareas sizes now right in terms of size based of max length
    
    * Fix(): textareas sizes now right in terms of size based of max length
    
    * Fix(): textareas sizes now right in terms of size based of max length

--- src/Models/Elements/Textarea.cs
+++ src/Models/Elements/Textarea.cs
@@ -35,24 +35,20 @@ namespace form_builder.Models.Elements
                 { "value", Properties.Value},
                 { "spellcheck", Properties.Spellcheck.ToString().ToLower() }
             };
-            
+
             properties.Add("maxlength", Properties.MaxLength);
-            if (Properties.MaxLength <= 200 || Properties.MaxLength > 500)
+            if (Properties.MaxLength >= 200)
             {
                 properties.Add("rows", Properties.MaxLength > 500 ? "15" : "5");
             }
 
-            if (DisplayAriaDescribedby)
-            {
-                if(DisplayCharacterCount)
-                {
-                    properties.Add("aria-describedby", $"{GetCustomItemId("info")} {GetDescribedByAttributeValue()}");
-                }
-                else
-                {
-                    properties.Add("aria-describedby", GetDescribedByAttributeValue());
-                }    
-            }
+            if (!DisplayAriaDescribedby)
+                return properties;
+
+            properties.Add("aria-describedby",
+                DisplayCharacterCount
+                    ? $"{GetCustomItemId("info")} {GetDescribedByAttributeValue()}"
+                    : GetDescribedByAttributeValue());
 
             return properties;
         }
