cccb1b96233c124ca7531a6f72950bc77795dc6b
Elena Alexandra <elena.secara@stockport.gov.uk>
2020-06-09 13:12:19 +0100

    test(buttonTests): add tests for button default text

--- tests/UnitTests/Models/Elements/ButtonTests.cs
+++ tests/UnitTests/Models/Elements/ButtonTests.cs
@@ -20,7 +20,7 @@ namespace form_builder_tests.UnitTests.Models.Elements
         private readonly Mock<IViewRender> _mockIViewRender = new Mock<IViewRender>();
         private readonly Mock<IElementHelper> _mockElementHelper = new Mock<IElementHelper>();
         private readonly Mock<IHostingEnvironment> _mockHostingEnv = new Mock<IHostingEnvironment>();
-
+        
         [Fact]
         public async Task RenderAsync_ShouldUseAddressSearchText_ForButton_WhenAddressSearch()
         {
@@ -69,13 +69,49 @@ namespace form_builder_tests.UnitTests.Models.Elements
                 .WithType(EElementType.Button)
                 .Build();
 
-            var textBoxEleement = new ElementBuilder()
+            var textBoxElement = new ElementBuilder()
                 .WithType(EElementType.Textbox)
                 .Build();
 
             var page = new PageBuilder()
-                .WithElement(textBoxEleement)
+                .WithElement(textBoxElement)
                 .WithElement(element)
+                .Build();
+
+
+            var schema = new FormSchemaBuilder()
+                .WithName("form-name")
+                .Build();
+
+            var viewModel = new Dictionary<string, dynamic>();
+
+            //Act
+            await element.RenderAsync(_mockIViewRender.Object, _mockElementHelper.Object, "", new List<AddressSearchResult>(), new List<OrganisationSearchResult>(), viewModel, page, schema, _mockHostingEnv.Object);
+
+            //Assert
+            Assert.Equal(SystemConstants.NextStepButtonText, callback.Properties.Text);
+            _mockIViewRender.Verify(_ => _.RenderAsync(It.Is<string>(x => x == "Button"), It.IsAny<Button>(), It.IsAny<Dictionary<string, object>>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task RenderAsync_ShouldUseDefaultSubmitButtonText_WhenSubmitFormBehaviour()
+        {
+            var callback = new Button();
+            _mockIViewRender.Setup(_ => _.RenderAsync(It.IsAny<string>(), It.IsAny<Button>(), It.IsAny<Dictionary<string, dynamic>>()))
+                .Callback<string, Button, Dictionary<string, dynamic>>((a, b, c) => callback = b);
+
+            //Arrange
+            var element = new ElementBuilder()
+                .WithType(EElementType.Button)
+                .Build();
+
+            var behaviour = new BehaviourBuilder()
+                .WithBehaviourType(EBehaviourType.SubmitForm)
+                .Build();
+
+            var page = new PageBuilder()
+                .WithElement(element)
+                .WithBehaviour(behaviour)
                 .Build();
 
             var schema = new FormSchemaBuilder()
@@ -85,13 +121,47 @@ namespace form_builder_tests.UnitTests.Models.Elements
             var viewModel = new Dictionary<string, dynamic>();
 
             //Act
-            var result = await element.RenderAsync(_mockIViewRender.Object, _mockElementHelper.Object, "", new List<AddressSearchResult>(), new List<OrganisationSearchResult>(), viewModel, page, schema, _mockHostingEnv.Object);
+            await element.RenderAsync(_mockIViewRender.Object, _mockElementHelper.Object, "", new List<AddressSearchResult>(), new List<OrganisationSearchResult>(), viewModel, page, schema, _mockHostingEnv.Object);
 
             //Assert
-            Assert.Equal(SystemConstants.NextStepButtonText, callback.Properties.Text);
+            Assert.Equal(SystemConstants.SubmitButtonText, callback.Properties.Text);
             _mockIViewRender.Verify(_ => _.RenderAsync(It.Is<string>(x => x == "Button"), It.IsAny<Button>(), It.IsAny<Dictionary<string, object>>()), Times.Once);
         }
 
+        [Fact]
+        public async Task RenderAsync_ShouldUseDefaultSubmitButtonText_WhenSubmitAndPayBehaviour()
+        {
+            var callback = new Button();
+            _mockIViewRender.Setup(_ => _.RenderAsync(It.IsAny<string>(), It.IsAny<Button>(), It.IsAny<Dictionary<string, dynamic>>()))
+                .Callback<string, Button, Dictionary<string, dynamic>>((a, b, c) => callback = b);
+
+            //Arrange
+            var element = new ElementBuilder()
+                .WithType(EElementType.Button)
+                .Build();
+
+            var behaviour = new BehaviourBuilder()
+                .WithBehaviourType(EBehaviourType.SubmitAndPay)
+                .Build();
+
+            var page = new PageBuilder()
+                .WithElement(element)
+                .WithBehaviour(behaviour)
+                .Build();
+
+            var schema = new FormSchemaBuilder()
+                .WithName("form-name")
+                .Build();
+
+            var viewModel = new Dictionary<string, dynamic>();
+
+            //Act
+            await element.RenderAsync(_mockIViewRender.Object, _mockElementHelper.Object, "", new List<AddressSearchResult>(), new List<OrganisationSearchResult>(), viewModel, page, schema, _mockHostingEnv.Object);
+
+            //Assert
+            Assert.Equal(SystemConstants.SubmitButtonText, callback.Properties.Text);
+            _mockIViewRender.Verify(_ => _.RenderAsync(It.Is<string>(x => x == "Button"), It.IsAny<Button>(), It.IsAny<Dictionary<string, object>>()), Times.Once);
+        }
 
         [Fact]
         public async Task RenderAsync_ShouldUse_SuppliedButtonText_DefaultButtonText()
