e0dadbf014cf60c169b14491f10d2a131dd8622f
sim.estill <simon.estill@gmail.com>
2020-06-11 14:29:46 +0100

    Added changes to summary added ui test for summary

---
+++ form-builder-tests-ui/StepDefinitions/SummarySteps.cs
@@ -0,0 +1,30 @@
+﻿using Coypu;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace form_builder_tests_ui.StepDefinitions
+{
+    [Binding, Scope(Tag = "summary")]
+    class SummarySteps : UiTestBase
+    {
+        [Then(@"I fill in page1")]
+        [When(@"I fill in page1")]
+        public void ThenIFillInPage1()
+        {
+            BrowserSession.FillIn("firstName").With("first");
+            BrowserSession.FillIn("lastName").With("last");
+            BrowserSession.FillIn("email").With("test@mail.com");
+            BrowserSession.FillIn("phone").With("01614451234");
+        }
+
+
+        [Then(@"I fill in page3")]
+        [When(@"I fill in page3")]
+        public void ThenIFillInPage3()
+        {
+            BrowserSession.FillIn("dob-day").With("01");
+            BrowserSession.FillIn("dob-month").With("02");
+            BrowserSession.FillIn("dob-year").With("1990");
+        }
+    }
+}
--- src/Models/Elements/Summary.cs
+++ src/Models/Elements/Summary.cs
@@ -1,27 +1,15 @@
-﻿using form_builder.Cache;
-using form_builder.Enum;
+﻿using form_builder.Enum;
 using form_builder.Helpers;
 using form_builder.Helpers.ElementHelpers;
-using form_builder.Models;
-using form_builder.Providers.StorageProvider;
-using form_builder.Builders;
-using Newtonsoft.Json;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using StockportGovUK.NetStandard.Models.Addresses;
 using StockportGovUK.NetStandard.Models.Verint.Lookup;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.IO;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Html;
 using System.Text.Encodings.Web;
-using Microsoft.Extensions.DependencyInjection;
-using form_builder.Mappers;
+
 
 namespace form_builder.Models.Elements
 {
@@ -32,7 +20,6 @@ namespace form_builder.Models.Elements
             Type = EElementType.Summary;
         }
 
-
         public override Task<string> RenderAsync(IViewRender viewRender, IElementHelper elementHelper, string guid, List<AddressSearchResult> addressSearchResults, List<OrganisationSearchResult> organisationResults, Dictionary<string, dynamic> viewModel, Page page, FormSchema formSchema, IHostingEnvironment environment)
         {
 
@@ -42,18 +29,16 @@ namespace form_builder.Models.Elements
             foreach (var pageSummary in pages)
             { 
                 htmlContent.AppendHtmlLine("<dl class=\"govuk-summary-list govuk-!-margin-bottom-9\">");
-                htmlContent.AppendHtml($"<h2 class=\"govuk-heading-m\">{pageSummary.PageTitle}</h2>");
-                foreach (var answer in pageSummary.Answers)
+               foreach (var answer in pageSummary.Answers)
                 {
                     htmlContent.AppendHtmlLine("<div class=\"govuk-summary-list__row\">");
                     htmlContent.AppendHtmlLine($"<dt class=\"govuk-summary-list__key\">{answer.Key}</dt>");
-                    htmlContent.AppendHtmlLine($"<dd class=\"govuk-summary-list__value\">{answer.Value}</dd>");
-                    htmlContent.AppendHtmlLine($"<dd class=\"govuk-summary-list__actions\"><a href=\"{pageSummary.PageSlug}\">Change <span class=\"govuk-visually-hidden\">{answer.Key}</span></a></dd>");
+                    htmlContent.AppendHtmlLine($"<dd class=\"govuk-summary-list__value\">{answer.Value}</dd>");                    
                     htmlContent.AppendHtmlLine("</div>");
                 }
                 htmlContent.AppendHtmlLine("</dl>");
 
-        }
+            }
            
             using (var writer = new StringWriter())
             {
