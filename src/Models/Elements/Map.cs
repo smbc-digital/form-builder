43cf2a98ab36b7e28d338d2d172436223d4877f7
Lydia Gray <lydia.gray@stockport.gov.uk>
2020-06-15 14:35:47 +0100

    feat(Map): add Map.cshtml and Map.cs

---
+++ src/Models/Elements/Map.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using form_builder.Enum;
+using form_builder.Helpers;
+using form_builder.Helpers.ElementHelpers;
+using Microsoft.AspNetCore.Hosting;
+using StockportGovUK.NetStandard.Models.Addresses;
+using StockportGovUK.NetStandard.Models.Verint.Lookup;
+
+namespace form_builder.Models.Elements
+{
+    public class Map : Element
+    {
+        public Map()
+        {
+            Type = EElementType.Map;
+        }
+    }
+}
