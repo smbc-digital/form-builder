8250a2fc624ff37c898fd37f2a71355129efaa1f
lallysmbc <stephen.lally.smbc@gmail.com>
2020-06-16 09:12:55 +0100

    debug(Org): Added log for org search result

--- src/Providers/Organisation/ServiceOrganisationProvider.cs
+++ src/Providers/Organisation/ServiceOrganisationProvider.cs
@@ -1,10 +1,10 @@
-using StockportGovUK.NetStandard.Gateways.VerintServiceGateway;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using StockportGovUK.NetStandard.Models.Verint.Lookup;
 using StockportGovUK.NetStandard.Gateways.OrganisationServiceGateway;
 using StockportGovUK.NetStandard.Models.Organisation;
 using StockportGovUK.NetStandard.Models.Enums;
+using Microsoft.Extensions.Logging;
 
 namespace form_builder.Providers.Organisation
 {
@@ -13,14 +13,19 @@ namespace form_builder.Providers.Organisation
         public string ProviderName => "CRM";
 
         private readonly IOrganisationServiceGateway _organisationServiceGateway;
+        private readonly ILogger<ServiceOrganisationProvider> _logger;
 
-        public ServiceOrganisationProvider(IOrganisationServiceGateway organisationServiceGateway)
+        public ServiceOrganisationProvider(IOrganisationServiceGateway organisationServiceGateway, ILogger<ServiceOrganisationProvider> logger)
         {
             _organisationServiceGateway = organisationServiceGateway;
+            _logger = logger;
         }
         public async Task<IEnumerable<OrganisationSearchResult>> SearchAsync(string organisation)
         {
-            var result = await _organisationServiceGateway.SearchAsync(new OrganisationSearch { OrganisationProvider = EOrganisationProvider.CRM, SearchTerm = organisation,  });
+            var result = await _organisationServiceGateway.SearchAsync(new OrganisationSearch { OrganisationProvider = EOrganisationProvider.CRM, SearchTerm = organisation });
+
+            _logger.LogDebug(Newtonsoft.Json.JsonConvert.SerializeObject(result));
+
             return result.ResponseContent;
         }
     }
