52a8b637c8603c703fd426c4f154e387c7f4dd33
lallysmbc <stephen.lally.smbc@gmail.com>
2020-07-02 12:16:36 +0100

    chore(RetrieveExternalDataService): added check for response statuscode

--- src/Services/RetrieveExternalDataService/RetrieveExternalDataService.cs
+++ src/Services/RetrieveExternalDataService/RetrieveExternalDataService.cs
@@ -7,15 +7,17 @@ using System.Threading.Tasks;
 using form_builder.Helpers.Session;
 using form_builder.Providers.StorageProvider;
 using Newtonsoft.Json;
+using System.Net.Http;
+using System;
 
 namespace form_builder.Services.RetrieveExternalDataService
 {
     public class RetrieveExternalDataService : IRetrieveExternalDataService
     {
         private readonly IGateway _gateway;
-        private Regex _tagRegex => new Regex("(?<={{).*?(?=}})", RegexOptions.Compiled);
         private readonly ISessionHelper _sessionHelper;
         private readonly IDistributedCacheWrapper _distributedCache;
+        private Regex _tagRegex => new Regex("(?<={{).*?(?=}})", RegexOptions.Compiled);
 
         public RetrieveExternalDataService(IGateway gateway, ISessionHelper sessionHelper, IDistributedCacheWrapper distributedCache)
         {
@@ -30,16 +32,22 @@ namespace form_builder.Services.RetrieveExternalDataService
             var formAnswers = _distributedCache.GetString(sessionGuid);
             var convertedFormAnswers = JsonConvert.DeserializeObject<FormAnswers>(formAnswers);
 
-            actions.ForEach((action) =>
+            actions.ForEach(async (action) =>
             {
-                var result = GenerateUrl(action.Properties.URL, convertedFormAnswers);
-                if (result.IsPost)
+                var response = new HttpResponseMessage();
+                var entity = GenerateUrl(action.Properties.URL, convertedFormAnswers);
+                _gateway.ChangeAuthenticationHeader("GET-FROM-CONFIG");
+
+                if (entity.IsPost)
                 {
-                    _gateway.PostAsync(result.Url, new { } );
-                    return;
+                    response = await _gateway.PostAsync(entity.Url, new { } );
+                } else {
+                    response = await _gateway.GetAsync(entity.Url);
                 }
 
-                _gateway.GetAsync(result.Url);
+                if(response.IsSuccessStatusCode)
+                    throw new ApplicationException($"RetrieveExternalDataService::Process, http request to {entity.Url} returned an unsuccessful status code, Response: {Newtonsoft.Json.JsonConvert.SerializeObject(response)}");
+
             });
             //loop over actions
             //identity httpmethod
