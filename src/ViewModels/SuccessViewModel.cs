7f563c4532f6a84d365e2dd68709e0d77dd6d5c1
sim.estill <simon.estill@gmail.com>
2020-07-20 13:59:03 +0100

    Fixed merge conflicts

--- src/ViewModels/SuccessViewModel.cs
+++ src/ViewModels/SuccessViewModel.cs
@@ -1,9 +1,5 @@
 using form_builder.Models;
 using System.Collections.Generic;
-<<<<<<< HEAD
-=======
-using form_builder.Models;
->>>>>>> design-system
 
 namespace form_builder.ViewModels
 {
@@ -15,21 +11,14 @@ namespace form_builder.ViewModels
 
         public string FeedbackFormUrl { get; set; }
 
-<<<<<<< HEAD
-        public FormAnswers FormAnswers { get; set; }
-=======
         public FormAnswers FormAnswers {get; set;}
->>>>>>> design-system
-
+>
         public string Reference { get; set; }
 
         public string PageContent { get; set; }
 
-<<<<<<< HEAD
-        public string StartFormUrl { get; set; }
-=======
         public string StartPageUrl { get; set; }
->>>>>>> design-system
+
 
         public bool HideBackButton => true;
 
@@ -38,17 +27,9 @@ namespace form_builder.ViewModels
         public string BannerTitle { get; set; }
 
         public string LeadingParagraph { get; set; }
-<<<<<<< HEAD
 
-=======
-      
->>>>>>> design-system
         public bool DisplayBreadcrumbs { get; set; }
 
         public List<Breadcrumb> Breadcrumbs{get; set;}
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> design-system
