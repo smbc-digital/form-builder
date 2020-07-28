41e46a3f62f988dd57fafb41c0a7cd1e0a401feb
Elena Alexandra <elena.secara@stockport.gov.uk>
2020-07-28 09:55:44 +0100

    chore(address): add test

--- ui-tests-cypress/cypress/integration/Address/index.js
+++ ui-tests-cypress/cypress/integration/Address/index.js
@@ -1,50 +1,18 @@
 describe('Address', () => {
   it('Address search', () => {
-    cy.visit('https://localhost:44360/address');
+    cy.visit('https://localhost:5000/address');
     cy.matchImageSnapshot('addressSearch');
   });
 
   it('Address Select', () => {
-    cy.visit('https://localhost:44360/address');
+    cy.visit('https://localhost:5000/address');
     cy.get('.govuk-input').type('sk11aa');
     cy.get('.govuk-button').click();
     cy.matchImageSnapshot('addressSelect');
   });
+
+  it('Address Manual', () => {
+    cy.visit('https://localhost:5000/address/page1/manual');
+    cy.matchImageSnapshot('addressManual');
+  });
 });
-
-
-
-
-// it('Address Select', async () => {
-//   const page = await browser.newPage();
-//   await page.goto('https://localhost:5000');
-
-//   const elementHandle = await page.$('.govuk-input');
-//   await elementHandle.type('sk11aa');
-
-//   await Promise.all([
-//     page.click("button"),
-//     page.waitForNavigation({ waitUntil: 'networkidle0' }),
-//   ]);
-
-//   const image = await page.screenshot({ fullPage: true});
-
-//   expect(image).toMatchImageSnapshot();
-// });
-
-// it('Address Manual', async () => {
-//   const page = await browser.newPage();
-//   await page.goto('https://localhost:5000');
-
-//   const elementHandle = await page.$('.govuk-input');
-//   await elementHandle.type('sk22aa');
-
-//   await Promise.all([
-//     page.click("button"),
-//     page.waitForNavigation({ waitUntil: 'networkidle0' }),
-//   ]);
-
-//   const image = await page.screenshot({ fullPage: true });
-
-//   expect(image).toMatchImageSnapshot();
-// });
