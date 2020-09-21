0bccc629e27b23582195586ba4c4a0ae90cbe280
lallysmbc <35955528+lallysmbc@users.noreply.github.com>
2020-09-21 10:45:55 +0100

    Develop (#210)
    * chore(spellcheck): Fixed spellcheck on textbox (#197)
    
    * fix(sumary): fixed check on summary, added test coverage (#196)
    
    * Back button (#198)
    
    * chore (backButton): Add Conditional check for the street/address lookup
    
    * refactor: remove comments
    
    * Add 1 git-crypt collaborator
    
    New collaborators:
    
    	77EEC75D Harry Woodall <harry.woodall@stockport.gov.uk>
    
    * Add 1 git-crypt collaborator
    
    New collaborators:
    
    	CC7E09D5 michael hayes <michael.hayes@stockport.gov.uk>
    
    * Add 1 git-crypt collaborator
    
    New collaborators:
    
    	C77B8DD7 Harry Woodall <harry.woodall@stockport.gov.uk>
    
    * chore(gpg): removed duplicate keys
    
    * Add 1 git-crypt collaborator
    
    New collaborators:
    
    	CE26A9D8 Harry Woodall <harry.woodall@stockport.gov.uk>
    
    * chore(page): use supplied page value if not auto/manual for backbutton
    
    * chore(GetReducedAnswers): update recursive method to support multiple pages with same pageslug (#203)
    
    * chore: Added maxlength override for reusable elements (#205)
    
    * chore(ReusableElementsFactory): updated reusable elemnts to allow for override of maxlength
    
    * tests(TransformFactory): added test for maxLength override if supplied
    
    * chore(ReusableElementFactory): Added override support for Hint and CustomValidationMessage (#206)
    
    * fix(Email): changed email element validation message to always be govuk standard errr (#208)
    
    * feat: resuable elements multiple pages (#209)
    
    * fix(ReusableElementFactory): updated factory to support pages with same page slug
    
    * tests(ReusableElementFactory): updated test setup
    
    Co-authored-by: michaellau-smbc <62244533+michaellau-smbc@users.noreply.github.com>
    Co-authored-by: Michael Hayes <michael.hayes@stockport.gov.uk>

--- src/Constants/ValidationConstants.cs
+++ src/Constants/ValidationConstants.cs
@@ -3,9 +3,8 @@
     public class ValidationConstants
     {
         public const string DatePickerDefault = "Check the date and try again";
-
         public const string POSTCODE_INCORRECT_FORMAT = "Enter a postcode in the correct format";
-
         public const string POSTCODE_EMPTY = "Enter the postcode";
+        public const string EMAIL_INCORRECT_FORMAT = "Enter an email address in the correct format, like name@example.com";
     }
 }
