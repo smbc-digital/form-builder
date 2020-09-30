8dc6f62b8787534e41064a44be920f3902b89bfd
lallysmbc <stephen.lally.smbc@gmail.com>
2020-09-30 13:26:46 +0100

    chore(Constants): change default max file size to 10Mb

--- src/Constants/SystemConstants.cs
+++ src/Constants/SystemConstants.cs
@@ -6,7 +6,7 @@ namespace form_builder.Constants
     {
         public static readonly List<string> AcceptedMimeTypes = new List<string>() { ".png", ".jpg", ".jpeg", ".pdf", ".docx", ".doc", ".odt" };
 
-        public static readonly int DefaultMaxFileSize = 24117248;
+        public static readonly int DefaultMaxFileSize = 10485760;
 
         public static readonly int DefaultMaxCombinedFileSize = 24117248;
     }
