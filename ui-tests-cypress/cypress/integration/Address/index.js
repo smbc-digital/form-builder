80b6dff92d4cc440fef12d5fe82bdc6ac3e2a984
lallysmbc <stephen.lally.smbc@gmail.com>
2020-10-12 12:13:56 +0100

    chore(cypress): updated ui test project

--- ui-tests-cypress/cypress/integration/Address/index.js
+++ ui-tests-cypress/cypress/integration/Address/index.js
@@ -1,18 +1,18 @@
 describe('Address', () => {
   it('Address search', () => {
-    cy.visit('https://localhost:5000/address');
+    cy.visit('https://localhost:5000/ui-address');
     cy.matchImageSnapshot('addressSearch');
   });
 
   it('Address Select', () => {
-    cy.visit('https://localhost:5000/address');
+    cy.visit('https://localhost:5000/ui-address');
     cy.get('.govuk-input').type('sk11aa');
     cy.get('.govuk-button').click();
     cy.matchImageSnapshot('addressSelect');
   });
 
   it('Address Manual', () => {
-    cy.visit('https://localhost:5000/address/page1/manual');
+    cy.visit('https://localhost:5000/ui-address/page1/manual');
     cy.matchImageSnapshot('addressManual');
   });
 });
