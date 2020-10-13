fddbcd9521702f2b8b7073d58a752640afc6dd0e
Lydia Gray <lydia.gray@stockport.gov.uk>
2020-10-13 10:05:05 +0100

    feat(FileUploadComparator): add FileUploadComparator

---
+++ src/Conditions/FileUploadComparator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using form_builder.Constants;
+using form_builder.Models;
+
+namespace form_builder.Conditions
+{
+    public static class FileUploadComparator
+    {
+        public static bool IsFileUploadNullOrEmpty(Condition condition, Dictionary<string, dynamic> viewModel)
+        {
+            var val = !string.IsNullOrEmpty(condition.ComparisonValue) ? bool.Parse(condition.ComparisonValue) : condition.IsNullOrEmpty;
+            var questionId = condition.QuestionId += FileUploadConstants.SUFFIX;
+
+            return viewModel.ContainsKey(questionId) && (viewModel[questionId] == null) == val;
+        }
+    }
+}
