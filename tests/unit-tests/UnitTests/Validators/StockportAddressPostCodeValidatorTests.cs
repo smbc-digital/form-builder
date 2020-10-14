30180ad252e0f29d7881271343abb8afa8e7c5aa
lallysmbc <35955528+lallysmbc@users.noreply.github.com>
2020-10-14 12:00:39 +0100

    feat(DIGITAL-3340): visual-tests suite (#272)
    * chore(cypress): add visual tests using cypress
    
    * chore(address): add test
    
    * chore(tests): add tests
    
    * chore(cypress): updated ui test project
    
    * chore(project): organised tests subprojects structure
    
    * chore(visual-tests): removed video files
    
    * chore(gitignore): updated ignore for cypress videos
    
    * tests(address): updated manual address
    
    * chore(tests): updated package name
    
    * chore(config): moved baseUrl to cypress config file
    
    * chore(project): removed unused fixtures
    
    * chore(visual-tests): moved dependencies
    
    * chore(workflow): updated dotnet test command
    
    Co-authored-by: Elena Alexandra <elena.secara@stockport.gov.uk>

---
+++ tests/unit-tests/UnitTests/Validators/StockportAddressPostCodeValidatorTests.cs
@@ -0,0 +1,89 @@
+﻿using form_builder.Builders;
+using form_builder.Enum;
+using form_builder.Validators;
+using System.Collections.Generic;
+using Xunit;
+
+namespace form_builder_tests.UnitTests.Validators
+{
+    public class StockportAddressPostCodeValidatorTests
+    {
+        private readonly StockportAddressPostcodeElementValidator _stockportPostcodeValidator = new StockportAddressPostcodeElementValidator();
+        
+        [Fact]
+        public void Validate_ShouldReturnTrue_WhenDoesNotPostcode()
+        {
+            // Arrange
+            var element = new ElementBuilder()
+                .WithType(EElementType.Address)
+                .Build();
+
+            var viewModel = new Dictionary<string, dynamic>();
+
+            // Act
+            var result = _stockportPostcodeValidator.Validate(element, viewModel);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+
+        [Fact]
+        public void Validate_ShouldValidatePostcode_WhenPostcodeSupplied()
+        {
+            // Arrange
+            var element = new ElementBuilder()
+                .WithQuestionId("testaddress")
+                .WithType(EElementType.Address)
+                .Build();
+
+            var viewModel = new Dictionary<string, dynamic>();
+            viewModel.Add("testaddress-postcode", "SK4 1AA");
+
+            // Act
+            var result = _stockportPostcodeValidator.Validate(element, viewModel);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_ShouldNotValidatePostcode_WhenInvalidPostcodeSupplied()
+        {
+            // Arrange
+            var element = new ElementBuilder()
+                .WithQuestionId("testaddress")
+                .WithType(EElementType.Address)
+                .WithStockportPostcode(true)
+                .Build();
+
+            var viewModel = new Dictionary<string, dynamic> {{"testaddress-postcode", "Elephant"}};
+
+            // Act
+            var result = _stockportPostcodeValidator.Validate(element, viewModel);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_ShouldNotValidatePostcode_WhenNonStockportPostcodeSupplied()
+        {
+            // Arrange
+            var element = new ElementBuilder()
+                .WithQuestionId("testaddress")
+                .WithType(EElementType.Address)
+                .WithStockportPostcode(true)
+                .Build();
+
+            var viewModel = new Dictionary<string, dynamic>();
+            viewModel.Add("testaddress-postcode", "OL16 0AE");
+
+            // Act
+            var result = _stockportPostcodeValidator.Validate(element, viewModel);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+    }
+}
