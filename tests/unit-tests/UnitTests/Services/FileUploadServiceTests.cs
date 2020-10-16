3747eeaf2010d9c179a36542a7609e9e3cca9505
michaellau-smbc <62244533+michaellau-smbc@users.noreply.github.com>
2020-10-16 11:44:24 +0100

    Chore: remove duplicate unit test method (#276)
    * Develop (#267)
    
    * document upload- merge work
    
    * feat(FileUploadComparator): add FileUploadComparator
    
    * test: add unit test for document upload condition check
    
    * chore(document-upload): amend redirect logic for incomplete urls, add recaptcha to disallowed keys
    
    * fix(pull request issues): fixes in FileUploadComparator, PageServiceTests, and ValidatorTests
    
    * fix(fileupload): fixed save issue with optional fileupload as final form page (#266)
    
    Co-authored-by: michaellau-smbc <michael.lau@stockport.gov.uk>
    Co-authored-by: Lydia Gray <lydia.gray@stockport.gov.uk>
    
    * Develop (#271)
    
    * document upload- merge work
    
    * feat(FileUploadComparator): add FileUploadComparator
    
    * test: add unit test for document upload condition check
    
    * chore(document-upload): amend redirect logic for incomplete urls, add recaptcha to disallowed keys
    
    * fix(pull request issues): fixes in FileUploadComparator, PageServiceTests, and ValidatorTests
    
    * fix(fileupload): fixed save issue with optional fileupload as final form page (#266)
    
    * fix: call endpoint and pass in document upload
    
    * fix: GetReducedAnswers method to handle empy string
    
    * refactor: GetReducedAnswers currentpage to use one tenary operator
    
    * refactor: GetReducedAnswer remove {}
    
    * chore(Validators): changed multi fileupload error message to check uploaded files count over invalid files count
    
    Co-authored-by: Lydia Gray <lydia.gray@stockport.gov.uk>
    Co-authored-by: lallysmbc <35955528+lallysmbc@users.noreply.github.com>
    Co-authored-by: lallysmbc <stephen.lally.smbc@gmail.com>
    
    * chore: remove duplicate unit test
    
    Co-authored-by: lallysmbc <35955528+lallysmbc@users.noreply.github.com>
    Co-authored-by: Lydia Gray <lydia.gray@stockport.gov.uk>
    Co-authored-by: lallysmbc <stephen.lally.smbc@gmail.com>

--- tests/unit-tests/UnitTests/Services/FileUploadServiceTests.cs
+++ tests/unit-tests/UnitTests/Services/FileUploadServiceTests.cs
@@ -375,46 +375,7 @@ namespace form_builder_tests.UnitTests.Services
             Assert.NotNull(result.Page);
             _mockPageHelper.Verify(_ => _.SaveAnswers(It.IsAny<Dictionary<string, dynamic>>(), It.IsAny<string>(), It.IsAny<string>(), fileUpload, true, true), Times.Once);
         }
-
-        [Fact]
-        public async Task ProcessFile_ProcessSelectedFiles_ShouldSave_WhenMUltipleFileUpload_IsOptional_WithNoFiles_OnSubmit()
-        {
-            // Arrange
-
-            var _element = new ElementBuilder()
-                .WithType(EElementType.MultipleFileUpload)
-                .WithQuestionId("fileUpload")
-                .WithOptional(true)
-                .Build();
-
-            var _page = new PageBuilder()
-                .WithElement(_element)
-                .WithValidatedModel(true)
-                .WithPageSlug("page-one")
-                .Build();
-
-            var _schema = new FormSchemaBuilder()
-                .WithPage(_page)
-                .WithBaseUrl("baseUrl")
-                .Build();
-
-            var viewModel = new Dictionary<string, dynamic>
-            {
-                {
-                    "Submit", "Submit"
-                }
-            };
-
-            // Act
-            var result = await _service.ProcessFile(viewModel, _page, _schema, new Guid().ToString(),
-                "path", null, true);
-
-            // Assert
-            Assert.IsType<ProcessRequestEntity>(result);
-            Assert.False(result.RedirectToAction);
-            _mockPageHelper.Verify(_ => _.SaveAnswers(It.IsAny<Dictionary<string, dynamic>>(), It.IsAny<string>(), It.IsAny<string>(), null, true, true), Times.Once);
-        }
-
+       
         [Fact]
         public async Task ProcessFile_ProcessSelectedFiles_ShouldCallPageFactory_IfModelStateInvalid()
         {
@@ -450,6 +411,52 @@ namespace form_builder_tests.UnitTests.Services
             _mockPageFactory.Verify(_ => _.Build(It.IsAny<Page>(), It.IsAny<Dictionary<string, dynamic>>(), It.IsAny<FormSchema>(), It.IsAny<string>(), It.IsAny<FormAnswers>(), null), Times.Once);
             Assert.IsType<ProcessRequestEntity>(result);
             Assert.True(result.UseGeneratedViewModel);
+        }
+
+        [Fact]
+        public async Task ProcessFile_ProcessSelectedFiles_ShouldSave_WhenMUltipleFileUpload_IsOptional_WithNoFiles_OnSubmit()
+        {
+
+            var _element = new ElementBuilder()
+                .WithType(EElementType.MultipleFileUpload)
+                .WithQuestionId("fileUpload")
+                .WithOptional(true)
+                .Build();
+
+            var _page = new PageBuilder()
+                .WithElement(_element)
+                .WithValidatedModel(true)
+                .WithPageSlug("page-one")
+                .Build();
+
+            var _schema = new FormSchemaBuilder()
+                .WithPage(_page)
+                .WithBaseUrl("baseUrl")
+                .Build();
+
+
+            // Arrange
+            var expectedRouteValues = new
+            {
+                form = "baseUrl",
+                path = "path"
+            };
+
+            var viewModel = new Dictionary<string, dynamic>
+            {
+                {
+                    "Submit", "Submit"
+                }
+            };
+
+            // Act
+            var result = await _service.ProcessFile(viewModel, _page, _schema, new Guid().ToString(),
+                "path", null, true);
+
+            // Assert
+            Assert.IsType<ProcessRequestEntity>(result);
+            Assert.False(result.RedirectToAction);
+            _mockPageHelper.Verify(_ => _.SaveAnswers(It.IsAny<Dictionary<string, dynamic>>(), It.IsAny<string>(), It.IsAny<string>(), null, true, true), Times.Once);
         }
     }
 }
