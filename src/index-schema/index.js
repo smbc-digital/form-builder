cc7002824453b2283aa109bcb1865e3905903c97
lallysmbc <stephen.lally.smbc@gmail.com>
2020-11-06 16:52:11 +0000

    refactor(indexSchema): removed call to s3, wrapped call with promise

--- src/index-schema/index.js
+++ src/index-schema/index.js
@@ -1,59 +1,38 @@
-const AWS = require('aws-sdk')
 const https = require('https');
 
-const key = ''
-const secret = ''
 const formbuildersecret = ''
 
-const getFormSchemas = (env, applicationVersion) =>
-    new Promise((resolve, reject) => {
-        const s3 = new AWS.S3({
-            region: 'eu-west-1',
-            credentials: new AWS.Credentials(key, secret)
-        })
+const updateCacheEntryWithinApplication2 = (host, applicationVersion) =>
+    new Promise(function (resolve, reject) {
+        var options = {
+            host: host,
+            port: '5000',
+            path: `${applicationVersion}/system/index-schemas?api_key=${formbuildersecret}`,
+            method: 'PATCH',
+            rejectUnauthorized: false //only required for localhost testing
+        };
 
-        const bucketParams = {
-            Bucket: 'forms-storage',
-            Prefix: `${env}/${applicationVersion}`
-        }
+        var req = https.request(options, function (res) {
+            if (res.statusCode !== 200)
+                return reject(new Error('Status Code:' + res.statusCode));
 
-        s3.listObjectsV2(bucketParams, (err, data) => {
-            if (err) {
-                reject(new Error('Unable to fetch list of files within forms-storage'))
-            } else if (data) {
-                resolve(data.Contents.map(_ => _.Key))
-            }
-        })
-    })
-
-const updateCacheEntryWithinApplication = () => {
-    var options = {
-        host: 'localhost',
-        port: '5000',
-        path: `/system/index-schemas?api_key=${formbuildersecret}`, //path needs /v1/v2 appended on int/qa/stage/prod
-        method: 'PATCH',
-        rejectUnauthorized: false //only required for localhost testing
-      };
-      
-      var req = https.request(options, function(res) {
-        console.log("Status code: ", res.statusCode);
-        res.on('data', function(d) {
-            if(res.statusCode !== 200){
-                throw new Error(d.toString())
-            }
+            res.on('data', function(chunk) {
+                resolve('Status code: '+res.statusCode + '\n\rResponse: '+ chunk.toString())
+            });
         });
-      });
-      
-      req.end();
-      
-      req.on('error', function(e) {
-        throw new Error(e)
-      });
+
+        req.end();
+
+        req.on('error', function(err) {
+            reject(err);
+        });
+    });
+
+async function init(host, applicationVersion) {
+    var result = await updateCacheEntryWithinApplication2(host, applicationVersion)
+    console.log(result)
 }
 
-async function init(env, applicationVersion) {
-    var results = await getFormSchemas(env, applicationVersion)
-    await updateCacheEntryWithinApplication(results)
-}
-
-init("Int", "v1")
+//Host: host int/qa/stage/prod.stockport.gov.uk
+//applicationVersion: empty string for local testing /v1 or /v2 - / required before version for path in http options above
+init("localhost", "")
