666c033fc93a29201b845661da9513b9fef6bfbb
Stuart Wilkes <stuart.wilkes@stockport.gov.uk>
2020-11-27 13:03:26 +0000

    feat(booking): Conditional for formatting date & previous month logic

--- src/Models/Elements/Booking.cs
+++ src/Models/Elements/Booking.cs
@@ -27,7 +27,9 @@ namespace form_builder.Models.Elements
         public AvailabilityDayResponse SelectedBooking;
         public bool IsSelectedAppointmentFullDay => SelectedBooking.IsFullDayAppointment;
         public bool IsAppointmentTypeFullDay => Appointments.Any(_ => _.IsFullDayAppointment);
-        public string AppointmentTypeFullDayIAG => $"You can select a date for {FormName} but you can not select a time. We’ll be with you between {DateTime.Today.Add(Appointments.FirstOrDefault().AppointmentTimes.First().StartTime).ToString("h:mm tt").Replace(" ", "").Replace(":00", "").ToLower()} and {DateTime.Today.Add(Appointments.FirstOrDefault().AppointmentTimes.First().EndTime).ToString("h:mm tt").Replace(" ", "").Replace(":00", "").ToLower()}.";
+        public string AppointmentStartTime { get; set; }
+        public string AppointmentEndTime { get; set; }
+        public string AppointmentTypeFullDayIAG => $"You can select a date for {FormName} but you can not select a time. We’ll be with you between {AppointmentStartTime} and {AppointmentEndTime}.";
         public string BookingDateQuestionId => $"{Properties.QuestionId}{BookingConstants.APPOINTMENT_DATE}";
         public DateTime CurrentSelectedMonth { get; set; }
         public DateTime FirstAvailableMonth { get; set; }
@@ -36,9 +38,7 @@ namespace form_builder.Models.Elements
         public DateTime NextSelectableMonth => new DateTime(CurrentSelectedMonth.Year, CurrentSelectedMonth.Month, 1).AddMonths(1);
         public DateTime PreviousSelectableMonth => new DateTime(CurrentSelectedMonth.Year, CurrentSelectedMonth.Month, 1).AddMonths(-1);
         public bool DisplayNextMonthArrow => NextSelectableMonth <= new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddMonths(Properties.SearchPeriod);
-        public bool DisplayPreviousMonthArrow => CurrentSelectedMonth.Year > DateTime.Now.Year 
-            ? true 
-            : CurrentSelectedMonth.Month > DateTime.Now.Month;
+        public bool DisplayPreviousMonthArrow => CurrentSelectedMonth > FirstAvailableMonth;
 
         public string FormName { get; set; }
 
@@ -69,9 +69,14 @@ namespace form_builder.Models.Elements
         }
         private void ConfigureDropDown(BookingInformation results)
         {
-            Appointments = results.Appointents;
+            Appointments = results.Appointments;
             CurrentSelectedMonth = results.CurrentSearchedMonth;
             FirstAvailableMonth = results.FirstAvailableMonth;
+            if (results.IsFullDayAppointment)
+            {
+                AppointmentStartTime = results.AppointmentStartTime;
+                AppointmentEndTime = results.AppointmentEndTime;
+            }
 
             SelectList.Add(new SelectListItem("selet date", string.Empty));
 
@@ -83,7 +88,7 @@ namespace form_builder.Models.Elements
 
         private AvailabilityDayResponse GetSelectedAppointment(BookingInformation results)
         {
-            Appointments = results.Appointents;
+            Appointments = results.Appointments;
             var selectedAppointment = Appointments.FirstOrDefault(_ => _.Date.ToString().Equals(Properties.Value));
 
             if (selectedAppointment == null)
