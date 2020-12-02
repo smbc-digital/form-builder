5777f9490cf22e64ffd89833b632a42bc39f7c6b
stephen.lally <stephen.lally.smbc@gmail.com>
2020-12-02 11:41:25 +0000

    chore(views): improved accessability for calendar

--- src/ViewModels/CalendarDayViewModel.cs
+++ src/ViewModels/CalendarDayViewModel.cs
@@ -7,5 +7,8 @@ namespace form_builder.ViewModels
         public string QuestionId { get; set; }
         public string CurrentValue { get; set; }
         public CalendarDay Day { get; set; }
+        public string InputId => $"{QuestionId}-{Day.Date.Day}";
+
+        public string GetCalendarHint() => $"{QuestionId}-{Day.Date.Day}-hint";
     }
 }
