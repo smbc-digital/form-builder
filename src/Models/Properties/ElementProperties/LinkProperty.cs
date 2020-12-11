780d28dbcfeeaf8fb01c883060555b4dc6a05e78
michaellau-smbc <michael.lau@stockport.gov.uk>
2020-12-11 16:43:04 +0000

    chore (Link) Add new link element

---
+++ src/Models/Properties/ElementProperties/LinkProperty.cs
@@ -0,0 +1,8 @@
+﻿namespace form_builder.Models.Properties.ElementProperties
+{
+    public partial class BaseProperty
+    {
+        public string Url { get; set; }
+        public bool OpenInTab { get; set; } = true;
+    }
+}
