fec8bc3f86e6e7cc219ad73a692994486cd471d9
stephen.lally <stephen.lally.smbc@gmail.com>
2021-01-06 14:32:24 +0000

    chore(views): rename propety:

--- src/ViewModels/TimeHeaderViewModel.cs
+++ src/ViewModels/TimeHeaderViewModel.cs
@@ -7,7 +7,7 @@ namespace form_builder.ViewModels
     public class TimeHeaderViewModel
     {
         public string DataAriaControlsIdForMorning { get; set; }
-        public string DataAriaControlsIdForEvening { get; set; }
+        public string DataAriaControlsIdForAfternoon { get; set; }
         public DateTime Date { get; set; }
         public string DateTextForNoScript => Date.ToFullDateFormat();
         public ETimePeriod SelectedTimePeriod { get; set; }
