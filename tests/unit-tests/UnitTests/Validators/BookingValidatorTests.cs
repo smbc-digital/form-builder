860b4f6502fe34f253bc59759414ed86d1ab97da
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2021-01-26 13:28:03 +0000

    fixed some missing merge conflicts

--- tests/unit-tests/UnitTests/Validators/BookingValidatorTests.cs
+++ tests/unit-tests/UnitTests/Validators/BookingValidatorTests.cs
@@ -131,7 +131,7 @@ namespace form_builder_tests.UnitTests.Validators
             };
 
             // Act
-            var result = _validator.Validate(element, viewModel);
+            var result = _validator.Validate(element, viewModel,  new form_builder.Models.FormSchema());
 
             // Assert
             Assert.False(result.IsValid);
@@ -153,7 +153,7 @@ namespace form_builder_tests.UnitTests.Validators
             };
 
             // Act
-            var result = _validator.Validate(element, viewModel);
+            var result = _validator.Validate(element, viewModel, new form_builder.Models.FormSchema());
 
             // Assert
             Assert.False(result.IsValid);
