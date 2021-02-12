ac2ab576bad4bbd17dec65637d60a4ef3abc3b9e
stephen.lally <stephen.lally.smbc@gmail.com>
2021-02-12 16:14:10 +0000

    fix(Action): Added jsonconvert attribute to EActionType for JsonSubTypes

--- src/Models/Actions/IAction.cs
+++ src/Models/Actions/IAction.cs
@@ -5,16 +5,18 @@ using form_builder.Models.Properties.ActionProperties;
 using form_builder.Providers.EmailProvider;
 using JsonSubTypes;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace form_builder.Models.Actions
 {
     [JsonConverter(typeof(JsonSubtypes), "Type")]
     public interface IAction
     {
+        [JsonConverter(typeof(StringEnumConverter))]
         EActionType Type { get; set; }
 
         BaseActionProperty Properties { get; set; }
-
+        
         Task Process(IActionHelper actionHelper, IEmailProvider emailProvider, FormAnswers formAnswers);
     }
 }
