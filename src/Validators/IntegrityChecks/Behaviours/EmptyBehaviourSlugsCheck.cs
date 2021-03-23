3f51104bb9b5c013f426780a8674e41bdf9a6f38
Stu <h.s.j.wilkes@gmail.com>
2021-03-23 14:12:59 +0000

    feat(clean_up_using_statements): reorder

--- src/Validators/IntegrityChecks/Behaviours/EmptyBehaviourSlugsCheck.cs
+++ src/Validators/IntegrityChecks/Behaviours/EmptyBehaviourSlugsCheck.cs
@@ -1,5 +1,5 @@
-using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using form_builder.Models;
 
 namespace form_builder.Validators.IntegrityChecks.Behaviours
--- tests/unit-tests/UnitTests/Validators/IntegrityChecks/ConditionalElementsAreValidCheckTests.cs
+++ tests/unit-tests/UnitTests/Validators/IntegrityChecks/ConditionalElementsAreValidCheckTests.cs
@@ -1,10 +1,8 @@
 using System.Collections.Generic;
-using System.Linq;
 using form_builder.Builders;
 using form_builder.Constants;
 using form_builder.Enum;
 using form_builder.Models;
-using form_builder.Validators.IntegrityChecks;
 using form_builder.Validators.IntegrityChecks.Form;
 using form_builder_tests.Builders;
 using Xunit;
