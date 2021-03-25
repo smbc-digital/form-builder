210ee91a8afbafe960b30fdd755a18b1dd754ff3
Harry <harry.woodall@stockport.gov.uk>
2021-03-25 11:06:22 +0000

    feat(auth): add 'Type' of auth token

---
+++ src/Extensions/GatewayExtentions.cs
@@ -0,0 +1,26 @@
+﻿using form_builder.Models;
+using form_builder.Services.MappingService.Entities;
+using StockportGovUK.NetStandard.Gateways;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace form_builder.Extensions {
+    public static class GatewayExtentions {
+
+        public async static Task<HttpResponseMessage> PostAsync(this IGateway gateway, MappingEntity mappingEntity, SubmitSlug submitSlug) {
+            switch (submitSlug.Type) {
+                case "flowtoken":
+                    return await gateway.PostAsync(submitSlug.URL, mappingEntity.Data, submitSlug.Type, submitSlug.AuthToken);
+                default:
+                    gateway.ChangeAuthenticationHeader(string.IsNullOrWhiteSpace(submitSlug.AuthToken)
+                    ? string.Empty
+                    : submitSlug.AuthToken);
+
+                    return await gateway.PostAsync(submitSlug.URL, mappingEntity.Data);
+            }
+        }
+    }
+}
