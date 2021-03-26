6532a1879cbfb4c17f4386c236d93f82b998ea8e
Harry <harry.woodall@stockport.gov.uk>
2021-03-26 13:45:15 +0000

    feat(): abstract submit logic behind provider, add powerApps submit provider, add tests

--- src/Services/SubmitService/SubmitService.cs
+++ src/Services/SubmitService/SubmitService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using form_builder.Configuration;
 using form_builder.Extensions;
@@ -7,6 +8,7 @@ using form_builder.Helpers.PageHelpers;
 using form_builder.Models;
 using form_builder.Providers.ReferenceNumbers;
 using form_builder.Providers.StorageProvider;
+using form_builder.Providers.Submit;
 using form_builder.Services.MappingService.Entities;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Options;
@@ -31,6 +33,8 @@ namespace form_builder.Services.SubmitService
 
         private readonly IReferenceNumberProvider _referenceNumberProvider;
 
+        private readonly IEnumerable<ISubmitProvider> _submitProviders;
+
         public SubmitService(
             IGateway gateway,
             IPageHelper pageHelper,
@@ -38,7 +42,8 @@ namespace form_builder.Services.SubmitService
             IOptions<SubmissionServiceConfiguration> submissionServiceConfiguration,
             IDistributedCacheWrapper distributedCache,  
             ISchemaFactory schemaFactory,
-            IReferenceNumberProvider referenceNumberProvider)
+            IReferenceNumberProvider referenceNumberProvider,
+            IEnumerable<ISubmitProvider> submitProviders)
         {
             _gateway = gateway;
             _pageHelper = pageHelper;
@@ -47,6 +52,7 @@ namespace form_builder.Services.SubmitService
             _distributedCache = distributedCache;
             _schemaFactory = schemaFactory;
             _referenceNumberProvider = referenceNumberProvider;
+            _submitProviders = submitProviders;
         }
 
         public async Task PreProcessSubmission(string form, string sessionGuid)
@@ -86,16 +92,11 @@ namespace form_builder.Services.SubmitService
             var currentPage = mappingEntity.BaseForm.GetPage(_pageHelper, mappingEntity.FormAnswers.Path);
             var submitSlug = currentPage.GetSubmitFormEndpoint(mappingEntity.FormAnswers, _environment.EnvironmentName.ToS3EnvPrefix());
 
-            System.Net.Http.HttpResponseMessage response;
-
-            var json = JsonConvert.SerializeObject(mappingEntity.Data);
-
-            response = await _gateway.PostAsync(mappingEntity, submitSlug);
+            System.Net.Http.HttpResponseMessage response = await _submitProviders.Get(submitSlug.Type).PostAsync(mappingEntity, submitSlug);
 
             if (!response.IsSuccessStatusCode)
                 throw new ApplicationException($"SubmitService::ProcessSubmission, An exception has occurred while attempting to call {submitSlug.URL}, Gateway responded with {response.StatusCode} status code, Message: {JsonConvert.SerializeObject(response)}");
             
-
             if (!baseForm.GenerateReferenceNumber && response.Content != null)
             {
                 var content = await response.Content.ReadAsStringAsync() ?? string.Empty;
---
+++ tests/unit-tests/UnitTests/Providers/Submit/AuthenticationHeaderSubmitProviderTests.cs
@@ -0,0 +1,76 @@
+﻿using form_builder.Builders;
+using form_builder.Enum;
+using form_builder.Helpers.PageHelpers;
+using form_builder.Models;
+using form_builder.Providers.Submit;
+using form_builder.Services.MappingService.Entities;
+using form_builder_tests.Builders;
+using Moq;
+using StockportGovUK.NetStandard.Gateways;
+using StockportGovUK.NetStandard.Gateways.Response;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace form_builder_tests.UnitTests.Providers.Submit {
+    public class AuthenticationHeaderSubmitProviderTests {
+
+        private readonly AuthenticationHeaderSubmitProvider _authenticationHeaderSubmitProvider;
+        private readonly Mock<IGateway> _mockGateway = new Mock<IGateway>();
+        private readonly Mock<IPageHelper> _mockPageHelper = new Mock<IPageHelper>();
+        private readonly MappingEntity _mappingEntity =
+            new MappingEntityBuilder()
+                .WithFormAnswers(new FormAnswers {
+                    Path = "page-one",
+                    Pages = new List<PageAnswers>
+                    {
+                        new PageAnswers
+                        {
+                            Answers = new List<Answers>
+                            {
+                                new Answers
+                                {
+                                    Response = "testResponse",
+                                    QuestionId = "testQuestionId"
+                                }
+                            },
+                            PageSlug = "page-one"
+                        }
+                    }
+                })
+                .WithData(new ExpandoObject())
+                .Build();
+
+        public AuthenticationHeaderSubmitProviderTests() {
+            _mockGateway.Setup(_ => _.PostAsync(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(new HttpResponseMessage { StatusCode = System.Net.HttpStatusCode.OK });
+
+            _authenticationHeaderSubmitProvider = new AuthenticationHeaderSubmitProvider(_mockGateway.Object);
+        }
+
+        [Fact]
+        public async Task PostAsync_ShouldCallGateway() {
+            SubmitSlug submitSlug = new SubmitSlug { URL = "www.test.com" };
+
+            await _authenticationHeaderSubmitProvider.PostAsync(_mappingEntity, submitSlug);
+
+            _mockGateway.Verify(_ => _.PostAsync(It.Is<string>(x => x == submitSlug.URL), It.Is<object>(x => x == _mappingEntity.Data)), Times.Once);
+        }
+
+        [Fact]
+        public async Task PostAsync_ShouldReturnResponseContent() {
+            SubmitSlug submitSlug = new SubmitSlug { URL = "www.test.com" };
+
+            var result = await _authenticationHeaderSubmitProvider.PostAsync(_mappingEntity, submitSlug);
+
+            Assert.NotNull(result);
+            Assert.IsType<HttpResponseMessage>(result);
+        }
+    }
+}
