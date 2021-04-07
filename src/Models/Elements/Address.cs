ba0843895b71afedfca64f29041c46d1b319838b
stephen.lally <stephen.lally.smbc@gmail.com>
2021-04-07 09:57:12 +0100

    chore(Element): move optional text to protected method

--- src/Models/Elements/Address.cs
+++ src/Models/Elements/Address.cs
@@ -45,7 +45,7 @@ namespace form_builder.Models.Elements
             }
         }
 
-        public override string GetLabelText(string pageTitle) => $"{(string.IsNullOrEmpty(Properties.SummaryLabel) ? pageTitle : Properties.SummaryLabel)}{(Properties.Optional ? " (optional)" : string.Empty)}";
+        public override string GetLabelText(string pageTitle) => $"{(string.IsNullOrEmpty(Properties.SummaryLabel) ? pageTitle : Properties.SummaryLabel)}{GetIsOptionalLabelText()}";
         
 
         public Address()
