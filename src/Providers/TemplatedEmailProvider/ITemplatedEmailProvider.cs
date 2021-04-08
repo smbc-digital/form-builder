de47b618dbfbf4828f9ebf942926ad3b8f081a70
michaellau-smbc <62244533+michaellau-smbc@users.noreply.github.com>
2021-04-08 14:43:44 +0100

    Fb with notify (#449)
    * feat(emailTemplate) add notify wip
    
    * feat(templated Email): update Action
    
    * chore(TemplatedEmailService) Add service for templatedEmail
    
    * feat(Templated email): wip
    
    * feat(Templated email): fix trailing comma on email list, amend reference used
    
    * refactor(ActionHelper): amend Aggregate on GetEmailToAddress to remove trailing comma
    
    * fix(test) fix exisiting tests
    
    * test(templatedEmail) Add unit tests
    
    * fix(templatedEmail) Add provider to service and bug fixes
    
    Co-authored-by: Lydia Gray <lydia.gray@stockport.gov.uk>

---
+++ src/Providers/TemplatedEmailProvider/ITemplatedEmailProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace form_builder.Providers.TemplatedEmailProvider
+{
+    public interface ITemplatedEmailProvider
+    {
+        public string ProviderName { get; }
+
+        Task SendEmailAsync(string emailAddress, string templateId, Dictionary<string, dynamic> personalisation);
+    }
+}
