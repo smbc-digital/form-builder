e5c370cbdacc2b7564b90fe2af6d73ae0a7b1fe0
michaellau-smbc <62244533+michaellau-smbc@users.noreply.github.com>
2021-04-09 09:31:48 +0100

    chore(IntegrityCheck) TemplatedEmail (#456)
    * chore(IntegrityCheck) TemplatedEmail
    
    * refactor(IntegrityCheck) remove curly and webhostenvirnoment
    
    * refactor(IntegrityCheck) update unit test by removing mock webhostenv
    
    * refactor(IntegrityCheck) remove mock webhostenv from constructor

---
+++ tests/unit-tests/UnitTests/Validators/IntegrityChecks/Form/TemplatedEmailActionCheckTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using form_builder.Enum;
+using form_builder.Models.Properties.ActionProperties;
+using form_builder.Validators.IntegrityChecks.Form;
+using form_builder_tests.Builders;
+using Moq;
+using Xunit;
+
+namespace form_builder_tests.UnitTests.Validators.IntegrityChecks.Form
+{
+    public class TemplatedEmailActionCheckTests
+    {
+        [Theory]
+        [InlineData("", "FAILURE - Templated Email Action, there is no 'EmailTemplateProvider'")]
+        [InlineData("questionId", "FAILURE - Templated Email Action, there is no 'TemplateId' provided")]
+        public void
+        TemplatedEmailActionCheck_ShouldThrowException_WhenActionDoesNotContain_Provider_or_TemplatedId(string questionId, string message)
+        {
+            // Arrange
+            var action = new ActionBuilder()
+                .WithActionType(EActionType.TemplatedEmail)
+                .WithTargetQuestionId(questionId)
+                .Build();
+
+            var schema = new FormSchemaBuilder()
+                .WithFormActions(action)
+                .Build();
+
+            var check = new TemplatedEmailActionCheck();
+
+            // Act & Assert
+            var result = check.Validate(schema);
+            Assert.False(result.IsValid);
+            Assert.Contains(message, result.Messages);
+        }
+    }
+}
