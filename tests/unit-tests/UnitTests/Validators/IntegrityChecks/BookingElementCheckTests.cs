48bb46eb1225e7d84f126c96e4f990e625b27120
Lydia Gray <lydia.gray@stockport.gov.uk>
2021-04-20 07:54:26 +0100

    refactor(BookingElementCheckTests): remove old Theory tests

--- tests/unit-tests/UnitTests/Validators/IntegrityChecks/BookingElementCheckTests.cs
+++ tests/unit-tests/UnitTests/Validators/IntegrityChecks/BookingElementCheckTests.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Hosting;
-using StockportGovUK.NetStandard.Models.Booking.Request;
 using form_builder.Builders;
 using form_builder.Constants;
 using form_builder.Enum;
@@ -9,7 +7,9 @@ using form_builder.Models;
 using form_builder.Models.Elements;
 using form_builder.Validators.IntegrityChecks.Elements;
 using form_builder_tests.Builders;
+using Microsoft.AspNetCore.Hosting;
 using Moq;
+using StockportGovUK.NetStandard.Models.Booking.Request;
 using Xunit;
 
 namespace form_builder_tests.UnitTests.Validators.IntegrityChecks
@@ -193,62 +193,6 @@ namespace form_builder_tests.UnitTests.Validators.IntegrityChecks
 
             // Act
             var result = _bookingElementCheck.Validate(element);
-
-            // Assert
-            Assert.False(result.IsValid);
-            Assert.Collection<string>(result.Messages, message => Assert.StartsWith(IntegrityChecksConstants.FAILURE, message));
-        }
-
-        [Theory]
-        [InlineData(EElementType.Booking, "local", "local", "69339C97-3924-477B-8D90-0986596072CE", "", "provider", true, 0, "69339C97-3924-477B-8D90-0986596072CE")]
-        [InlineData(EElementType.Booking, "local", "local", "69339C97-3924-477B-8D90-0986596072CE", "", "provider", true, 1, "00000000-0000-0000-0000-000000000000")]
-        [InlineData(EElementType.Booking, "local", "int", "69339C97-3924-477B-8D90-0986596072CE", "", "provider", false, 0, "00000000-0000-0000-0000-000000000000")]
-        [InlineData(EElementType.Booking, "local", "local", "00000000-0000-0000-0000-000000000000", "", "provider", false, 0, "00000000-0000-0000-0000-000000000000")]
-        [InlineData(EElementType.Booking, "local", "local", "69339C97-3924-477B-8D90-0986596072CE", "69339C97-3924-477B-8D90-0986596072CE", "provider", false, 0, "00000000-0000-0000-0000-000000000000")]
-        [InlineData(EElementType.Booking, "local", "local", "69339C97-3924-477B-8D90-0986596072CE", "", "", false, 0, "00000000-0000-0000-0000-000000000000")]
-
-        public void BookingElementCheck_ReturnInValid(
-            EElementType elementType,
-            string actualEnv,
-            string appointmentTypeEnv,
-            string appointmentId,
-            string appointmentIdKey,
-            string provider,
-            bool optionalResource,
-            int optionalResourceCount,
-            Guid optionalResourceId)
-        {
-            // Arrange
-            _mockHostingEnv.Setup(environment => environment.EnvironmentName).Returns(actualEnv);
-
-            var appointmentType = new AppointmentTypeBuilder()
-                .WithEnvironment(appointmentTypeEnv)
-                .WithAppointmentId(new Guid(appointmentId))
-                .WithAppointmentIdKey(appointmentIdKey)
-                .Build();
-
-            if (optionalResource)
-            {
-                appointmentType.OptionalResources = new List<BookingResource>
-                {
-                    new BookingResource
-                    {
-                        Quantity = optionalResourceCount,
-                        ResourceId = optionalResourceId
-                    }
-                };
-            }
-
-            var element = new ElementBuilder()
-               .WithType(elementType)
-               .WithQuestionId("booking")
-               .WithBookingProvider(provider)
-               .WithAppointmentType(appointmentType)
-               .Build();
-
-            // Act
-            var check = new BookingElementCheck(_mockHostingEnv.Object);
-            var result = check.Validate(element);
 
             // Assert
             Assert.False(result.IsValid);
