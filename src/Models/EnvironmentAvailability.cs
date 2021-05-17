9ccd2fac73cdf419195b0245be851cbea36ef792
michaellau-smbc <michael.lau@stockport.gov.uk>
2021-05-17 13:17:18 +0100

    chore(enabledFor) wip

--- src/Models/EnvironmentAvailability.cs
+++ src/Models/EnvironmentAvailability.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace form_builder.Models
 {
     public class EnvironmentAvailability
@@ -10,5 +13,18 @@ namespace form_builder.Models
         public string Environment { get; set; }
 
         public bool IsAvailable { get; set; }
+
+        public List<EnabledForBase> EnabledFor { get; set; }
+
+        public class EnabledForBase
+        {
+            public string Type { get; set; }
+        }
+
+        public class TimeWindow : EnabledForBase
+        {           
+            public DateTime Start { get; set; } = DateTime.MinValue;
+            public DateTime End { get; set; } = DateTime.MaxValue;
+        }
     }
 }
