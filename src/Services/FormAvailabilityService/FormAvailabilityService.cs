174e68dfcaf8a0469891f9ec9f199e339b13cbbf
stephen.lally <stephen.lally.smbc@gmail.com>
2021-05-17 16:33:09 +0100

    tests(FormAvailability)

--- src/Services/FormAvailabilityService/FormAvailabilityService.cs
+++ src/Services/FormAvailabilityService/FormAvailabilityService.cs
@@ -4,22 +4,22 @@ using form_builder.EnabledFor;
 using form_builder.Extensions;
 using form_builder.Models;
 
-namespace form_builder.Services.FormAvailabilityServics
+namespace form_builder.Services.FormAvailabilityService
 {
-    public interface IFormAvailabilityServics
+    public interface IFormAvailabilityService
     {
         bool IsAvailable(List<EnvironmentAvailability> availability, string environment);
     }
-    public class FormAvailabilityServics : IFormAvailabilityServics
+    public class FormAvailabilityService : IFormAvailabilityService
     {
         private readonly IEnumerable<IEnabledFor> _enabledFor;
-        public FormAvailabilityServics(IEnumerable<IEnabledFor> enabledFor) => _enabledFor = enabledFor;
+        public FormAvailabilityService(IEnumerable<IEnabledFor> enabledFor) => _enabledFor = enabledFor;
 
         public bool IsAvailable(List<EnvironmentAvailability> availability, string environment)
         {
             var environmentAvailability = availability.SingleOrDefault(_ => _.Environment.ToLower().Equals(environment.ToLower()));
 
-            if(environmentAvailability.EnabledFor is not null && environmentAvailability.EnabledFor.Any())
+            if(environmentAvailability is not null && environmentAvailability.EnabledFor is not null && environmentAvailability.EnabledFor.Any())
                 return environmentAvailability.EnabledFor.All(_ => _enabledFor.Get(_.Type).IsAvailable(_));
 
             return environmentAvailability == null || environmentAvailability.IsAvailable;
