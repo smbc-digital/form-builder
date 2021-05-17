1a4b6e345a2cbca3b36a8d9e2b7c8d750bbe41a8
stephen.lally <stephen.lally.smbc@gmail.com>
2021-05-17 16:33:09 +0100

    tests(FormAvailability)

---
+++ tests/unit-tests/UnitTests/EnabledFor/TimeWindowTests.cs
@@ -0,0 +1,58 @@
+using System;
+using form_builder.EnabledFor;
+using form_builder.Models;
+using form_builder.Models.Properties.EnabledForProperties;
+using Xunit;
+
+namespace form_builder_tests.UnitTests.Services
+{
+    public class TimeWindowTests
+    {
+        private readonly TimeWindow _timeWindow = new TimeWindow();
+
+        [Fact]
+        public void IsAvailable_ShouldReturnTrue_WhenDate_IsBetween_Start_And_End_Date()
+        {
+            var model = new EnabledForBase
+            {
+                Properties = new EnabledForProperties 
+                {
+                    Start = DateTime.Now.AddDays(-1),
+                    End = DateTime.Now.AddDays(+1)
+                }
+            };
+
+            Assert.True(_timeWindow.IsAvailable(model));
+        }
+
+        [Fact]
+        public void IsAvailable_ShouldReturnFalse_WhenDate_IsAfter_EndDate()
+        {
+            var model = new EnabledForBase
+            {
+                Properties = new EnabledForProperties 
+                {
+                    Start = DateTime.Now.AddDays(-2),
+                    End = DateTime.Now.AddDays(-1)
+                }
+            };
+            
+            Assert.False(_timeWindow.IsAvailable(model));
+        }
+
+        [Fact]
+        public void IsAvailable_ShouldReturnFalse_WhenDate_IsBefore_StartDate()
+        {
+            var model = new EnabledForBase
+            {
+                Properties = new EnabledForProperties 
+                {
+                    Start = DateTime.Now.AddDays(+1),
+                    End = DateTime.Now.AddDays(+2)
+                }
+            };
+            
+            Assert.False(_timeWindow.IsAvailable(model));
+        }
+    }
+}
