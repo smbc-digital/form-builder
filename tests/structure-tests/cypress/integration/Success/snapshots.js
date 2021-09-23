b6230656ad189ea6ddbc1c4d748a4df487642ddc
mhayesm <michael.hayes@stockport.gov.uk>
2021-09-23 15:01:48 +0100

    tests(structure): success page

--- tests/structure-tests/cypress/integration/Success/snapshots.js
+++ tests/structure-tests/cypress/integration/Success/snapshots.js
@@ -1,8 +1,8 @@
 module.exports = {
   "Success": {
     "Success": {
-      "1": "\n                    \n<div class=\"smbc-panel smbc-panel--confirmation\">\n    <h1 class=\"govuk-panel__title\">We've received your report</h1>\n\n        <div class=\"govuk-body-l\">Thanks for reporting a flood</div>\n\n        <div class=\"smbc-panel__body\">Your reference number is: <strong>123456</strong></div>\n</div>\n\n<h2 class=\"govuk-heading-l\">What happens next</h2>\n<p class=\"smbc-body\">If you've provided your email address you'll receive a confirmation email.</p>\n<p class=\"smbc-body\">We'll review your report and investigate further if needed.</p>\n<p class=\"smbc-body\">We'll monitor and assess flooding trends or wider issues. Flooding in a home, business or on private land is for the owner to resolve, however we can give you advice.</p>\n<p class=\"smbc-body\">If we need more information, we'll get in touch with you.</p>\n<p class=\"smbc-body\"><a href=\"https://www.stockport.gov.uk/\" class=\"govuk-button\">Go to the homepage</a></p>\n\n                "
+      "1": "\n                    \n<div class=\"smbc-panel smbc-panel--confirmation\">\n    <h1 class=\"govuk-panel__title\">We've received your report</h1>\n\n        <div class=\"govuk-body-l\">Thanks for reporting a flood</div>\n\n        <div class=\"smbc-panel__body smbc-!-font-word-break\">Your reference number is: <strong>123456</strong></div>\n</div>\n\n<h2 class=\"govuk-heading-m\">What happens next</h2>\n<p class=\"smbc-body\">If you've provided your email address you'll receive a confirmation email.</p>\n<p class=\"smbc-body\">We'll review your report and investigate further if needed.</p>\n<p class=\"smbc-body\">We'll monitor and assess flooding trends or wider issues. Flooding in a home, business or on private land is for the owner to resolve, however we can give you advice.</p>\n<p class=\"smbc-body\">If we need more information, we'll get in touch with you.</p>\n<p class=\"smbc-body\"><a href=\"https://www.stockport.gov.uk/\" class=\"govuk-button\">Go to the homepage</a></p>\n\n                "
     }
   },
-  "__version": "5.4.0"
+  "__version": "6.1.0"
 }
