c853b31f327f13f11093cbe72b4a204509908515
Yasmin Tubbritt <yasmin.tubbritt@stockport.gov.uk>
2021-10-07 15:08:36 +0100

    updated breadcrumb structure test

--- tests/structure-tests/cypress/integration/Breadcrumbs/snapshots.js
+++ tests/structure-tests/cypress/integration/Breadcrumbs/snapshots.js
@@ -4,5 +4,5 @@ module.exports = {
       "1": "\n    <ol class=\"govuk-breadcrumbs__list\">\n            <li class=\"govuk-breadcrumbs__list-item\"><a class=\"govuk-breadcrumbs__link\" href=\"https://www.stockport.gov.uk\">Home</a></li>\n            <li class=\"govuk-breadcrumbs__list-item\"><a class=\"govuk-breadcrumbs__link\" href=\"https://www.stockport.gov.uk/topic/bins-and-recycling\">Bins and recycling</a></li>\n    </ol>\n"
     }
   },
-  "__version": "5.4.0"
+  "__version": "6.1.0"
 }
