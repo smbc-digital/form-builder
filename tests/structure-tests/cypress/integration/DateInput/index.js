527557e5128d72d002dc91d3cb0ccd9d673b1b18
Michael Lau <michael.lau@stockport.gov.uk>
2021-10-21 11:48:30 +0100

    chore(structuretest): snapshot for each test case

--- tests/structure-tests/cypress/integration/DateInput/index.js
+++ tests/structure-tests/cypress/integration/DateInput/index.js
@@ -1,22 +1,22 @@
 describe('DateInput', () => {
     it('DateInput Required', () => {
       cy.visit('ui-date-input')
-        .toMatchingDOM('govuk-form-group')
+        .toMatchingDOM('govuk-form-group',0)
     });
   
     it('DateInput Optional', () => {
       cy.visit('ui-date-input')
-        .toMatchingDOM('govuk-form-group')
+        .toMatchingDOM('govuk-form-group',1)
     });
   
     it('DateInput Limitation', () => {
       cy.visit('ui-date-input')
-        .toMatchingDOM('govuk-form-group')
+        .toMatchingDOM('govuk-form-group',2)
     });
 
     it('DateInput Validation', () => {
       cy.visit('ui-date-input')
-        .get('.govuk-button').click()
+        .get('.govuk-button').click()        
         .toMatchingDOM('govuk-form-group')
     });
 });
