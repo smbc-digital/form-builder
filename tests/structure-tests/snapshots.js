d2857c8614fd0e9d2f0b6ee9054270f9fe36d2b6
Harry <harry.woodall@stockport.gov.uk>
2021-10-21 13:55:57 +0100

    tests(Declaration Structure): add 'no label' element, create new structure test

--- tests/structure-tests/snapshots.js
+++ tests/structure-tests/snapshots.js
@@ -33,6 +33,9 @@ module.exports = {
   "Declaration": {
     "Declaration": {
       "1": "\n    <fieldset aria-describedby=\"DeclarationWithHiddenTitle-hint\" class=\"govuk-fieldset\">\n\n            <legend class=\"govuk-fieldset__legend govuk-fieldset__legend--l\">\n        <h1 class=\"govuk-fieldset__heading\">\n            Required Declaration\n\n        </h1>\n    </legend>\n\n\n        <span class=\"govuk-hint\" id=\"DeclarationWithHiddenTitle-hint\">This is a required declaration</span>\n\n\n    \n\n    <div class=\"govuk-checkboxes\">\n        <div class=\"govuk-checkboxes__item\">\n            <input class=\"govuk-checkboxes__input\" type=\"checkbox\" value=\"true\" name=\"DeclarationWithHiddenTitle\" id=\"DeclarationWithHiddenTitle\">\n\n            <label class=\"govuk-label govuk-checkboxes__label\" for=\"DeclarationWithHiddenTitle\">I agree to the terms and conditions\n            </label>\n        </div>\n    </div>\n\n    </fieldset>\n"
+    },
+    "Declaration no label": {
+      "1": "\n    <fieldset aria-describedby=\"DeclarationWithNoLabel-hint\" class=\"govuk-fieldset\">\n\n\n        <span class=\"govuk-hint\" id=\"DeclarationWithNoLabel-hint\">This is a required declaration</span>\n\n\n    \n\n    <div class=\"govuk-checkboxes\">\n        <div class=\"govuk-checkboxes__item\">\n            <input class=\"govuk-checkboxes__input\" type=\"checkbox\" value=\"true\" name=\"DeclarationWithNoLabel\" id=\"DeclarationWithNoLabel\">\n\n            <label class=\"govuk-label govuk-checkboxes__label\" for=\"DeclarationWithNoLabel\">I agree to the terms and conditions\n            </label>\n        </div>\n    </div>\n\n    </fieldset>\n"
     }
   },
   "FileUpload": {
