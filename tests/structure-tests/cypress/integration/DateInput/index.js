cb3bba6950d18bef324ab16cd48bfea6f1b92923
Michael Lau <michael.lau@stockport.gov.uk>
2021-10-21 08:28:49 +0100

    chore(StructureTest) remove datepicker and update DateInput

--- tests/structure-tests/cypress/integration/DateInput/index.js
+++ tests/structure-tests/cypress/integration/DateInput/index.js
@@ -1,6 +1,22 @@
 describe('DateInput', () => {
-    it('DateInput', () => {
+    it('DateInput Required', () => {
       cy.visit('ui-date-input')
-        .toMatchingDOM('govuk-form-group')
+      .toMatchingDOM('govuk-form-group')
     });
-  });
+  
+    it('DateInput Optional', () => {
+      cy.visit('ui-date-input')
+      .toMatchingDOM('govuk-form-group')
+    });
+  
+    it('DateInput Limitation', () => {
+      cy.visit('ui-date-input')
+      .toMatchingDOM('govuk-form-group')
+    });
+
+    it('DateInput Validation', () => {
+      cy.visit('ui-date-input')
+      .get('.govuk-button').click()
+      .toMatchingDOM('govuk-form-group')
+    });
+});
