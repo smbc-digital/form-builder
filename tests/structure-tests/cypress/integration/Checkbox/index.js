2dc81b6fd9c5d9d447377f960b0a1bd53df9c009
Jon Chiles <jonchiles@yahoo.com>
2021-10-22 07:56:21 +0100

    chore(chckbox button): structure tests

--- tests/structure-tests/cypress/integration/Checkbox/index.js
+++ tests/structure-tests/cypress/integration/Checkbox/index.js
@@ -3,4 +3,20 @@ describe('Checkbox', () => {
       cy.visit('ui-checkbox')
         .toMatchingDOM('govuk-form-group')
     });
-  });
+
+    it('Checkbox validation', () => {
+      cy.visit('ui-checkbox')
+      cy.get('.govuk-button').click()
+        .toMatchingDOM()
+    });
+    
+    it('Checkbox optional', () => {
+      cy.visit('ui-checkbox-conditional')
+        .toMatchingDOM('govuk-form-group')
+    });
+
+    it('Checkbox conditional', () => {
+      cy.visit('ui-checkbox-conditional')
+        .toMatchingDOM('govuk-form-group')
+    });
+  }); 
