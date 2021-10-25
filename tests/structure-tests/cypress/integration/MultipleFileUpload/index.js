b4d977796456027d7edc500a8d265e2a7e6ebe87
Jordan <jordan.burns@stockport.gov.uk>
2021-10-25 16:25:26 +0100

    test(Multi File Upload): Add structure tests, add cypress-file-upload to package json

--- tests/structure-tests/cypress/integration/MultipleFileUpload/index.js
+++ tests/structure-tests/cypress/integration/MultipleFileUpload/index.js
@@ -1,6 +1,30 @@
 describe('Multiple file upload', () => {
-    it('Multiple file upload', () => {
-      cy.visit('ui-multiple-file-upload')
-        .toMatchingDOM()
-    });
+  it('Mandatory multiple file upload', () => {
+    cy.visit('ui-multiple-file-upload')
+    cy.get('#FileOptional-fileupload-SubmitButton').click()
+      .toMatchingDOM('govuk-form-group')
   });
+
+  it('Optional multiple file upload', () => {
+    cy.visit('ui-multiple-file-upload')
+      .toMatchingDOM('govuk-form-group')
+  });
+
+  it('File uploaded', () => {
+    const fileName = 'files/testImage.jpg'
+
+    cy.visit('ui-multiple-file-upload')
+    cy.get('#FileOptional-fileupload-SubmitButton').click()
+    cy.get('#File-fileupload').attachFile(fileName)
+    cy.wait(1000)
+    cy.get('#upload').click()
+      .toMatchingDOM('govuk-form-group')
+  });
+
+  it('No file uploaded validation', () => {
+    cy.visit('ui-multiple-file-upload')
+    cy.get('#FileOptional-fileupload-SubmitButton').click()
+    cy.get('#upload').click()
+      .toMatchingDOM('govuk-form-group')
+  });
+});
