9b43476dc806bfacb8a8d920c872ac69d9600b03
Priya Ganger <priya.ganger@stockport.gov.uk>
2021-10-25 15:42:56 +0100

    (structure tests): add address validation

--- tests/structure-tests/cypress/integration/Address/index.js
+++ tests/structure-tests/cypress/integration/Address/index.js
@@ -1,5 +1,5 @@
 describe('Address', () => {
-  it('Address search', () => {
+  it('Address Search', () => {
     cy.visit('ui-address')
       .toMatchingDOM()
   });
@@ -17,4 +17,27 @@ describe('Address', () => {
       .get('.govuk-button').click()
       .toMatchingDOM()
   });
+
+  it('Address Search Validation', () => {
+    cy.visit('ui-address')
+      .get('.govuk-button').click()
+      .toMatchingDOM()
+  });
+
+  it('Address Select Validation', () => {
+    cy.visit('ui-address')
+      .get('.govuk-input').type('sk11aa')
+      .get('.govuk-button').click()
+      .get('.govuk-button').click()
+      .toMatchingDOM()
+  });
+
+  it('Address Manual Validation', () => {
+    cy.visit('ui-address')
+      .get('.govuk-input').type('sk11aa')
+      .get('.govuk-button').click()
+      .get('.govuk-select').select("address 1")
+      .get('.govuk-button').click()
+      .toMatchingDOM()
+  });  
 });
