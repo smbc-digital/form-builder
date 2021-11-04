aea90f2b06382e3ccd4b91cc409994b598dc3d2b
mhayesm <michael.hayes@stockport.gov.uk>
2021-11-04 11:57:59 +0000

    chore(tests): remove comment & failing test from origin

--- tests/structure-tests/cypress/integration/DocumentDownloadButton/index.js
+++ tests/structure-tests/cypress/integration/DocumentDownloadButton/index.js
@@ -3,9 +3,4 @@ describe('DocumentDownloadButton', () => {
       cy.visit('ui-document-download')
         .toMatchingDOM('govuk-form-group', 0)
     });
-
-    // it('Declaration no label', () => {
-    //   cy.visit('ui-declaration')
-    //     .toMatchingDOM('govuk-form-group', 1)
-    // });
   });
