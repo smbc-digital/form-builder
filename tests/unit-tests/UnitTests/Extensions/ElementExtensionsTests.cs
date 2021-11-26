1e378bc7634f201fadcb6199d48c70a756e89fb0
Lydia Gray <lydia.gray@stockport.gov.uk>
2021-11-26 08:46:29 +0000

    fix(RemoveUnusedConditionalElements): fix for validating multiple elements with conditional elements

--- tests/unit-tests/UnitTests/Extensions/ElementExtensionsTests.cs
+++ tests/unit-tests/UnitTests/Extensions/ElementExtensionsTests.cs
@@ -14,6 +14,7 @@ namespace form_builder_tests.UnitTests.Extensions
         [Fact]
         public void RemoveUnusedConditionalElements_ViewModelShouldContainLessElementsWhenElementsContainMultipleConditionals_ForRadio()
         {
+            // Arrange
             List<IElement> elements = new List<IElement>();
             var conditionalElement1 = new ElementBuilder()
                 .WithType(EElementType.Textbox)
@@ -38,10 +39,17 @@ namespace form_builder_tests.UnitTests.Extensions
             elements.Add(conditionalElement1);
             elements.Add(conditionalElement2);
             elements.Add(element1);
-            var viewModel = new Dictionary<string, dynamic> { { "testQuestion", "Value1" }, { "conditionalQuestion1", "Conditional Answer 1" }, { "conditionalQuestion2", "Conditional Answer 2" } };
+            var viewModel = new Dictionary<string, dynamic>
+            {
+                { "testQuestion", "Value1" }, 
+                { "conditionalQuestion1", "Conditional Answer 1" }, 
+                { "conditionalQuestion2", "Conditional Answer 2" }
+            };
 
+            // Act
             List<IElement> listOfElements = elements.RemoveUnusedConditionalElements(viewModel);
 
+            // Assert
             Assert.Equal(2, listOfElements.Count);
             Assert.Equal(2, viewModel.Count);
         }
@@ -49,6 +57,7 @@ namespace form_builder_tests.UnitTests.Extensions
         [Fact]
         public void RemoveUnusedConditionalElements_ShouldKeepOriginalViewModelWhenNoConditionalElementsInList()
         {
+            // Arrange
             List<IElement> elements = new List<IElement>();
             var viewModel = new Dictionary<string, dynamic> { { "testQuestion", "Value" } };
             var element1 = new ElementBuilder()
@@ -56,8 +65,11 @@ namespace form_builder_tests.UnitTests.Extensions
                 .WithQuestionId("testQuestion")
                 .Build();
             elements.Add(element1);
+
+            // Act
             List<IElement> listOfElements = elements.RemoveUnusedConditionalElements(viewModel);
 
+            // Assert
             Assert.Single(listOfElements);
             Assert.Single(viewModel);
         }
@@ -65,6 +77,7 @@ namespace form_builder_tests.UnitTests.Extensions
         [Fact]
         public void RemoveUnusedConditionalElements_ViewModelShouldContain_SameAmountOf_ElementsWhen_Checkbox_Has_Selected_BothOptions()
         {
+            // Arrange
             List<IElement> elements = new List<IElement>();
             var conditionalElement1 = new ElementBuilder()
                 .WithType(EElementType.Textbox)
@@ -89,10 +102,17 @@ namespace form_builder_tests.UnitTests.Extensions
             elements.Add(conditionalElement1);
             elements.Add(conditionalElement2);
             elements.Add(element1);
-            var viewModel = new Dictionary<string, dynamic> { { "testQuestion", "Value1,Value2" }, { "conditionalQuestion1", "Conditional Answer 1" }, { "conditionalQuestion2", "Conditional Answer 2" } };
+            var viewModel = new Dictionary<string, dynamic>
+            {
+                { "testQuestion", "Value1,Value2" }, 
+                { "conditionalQuestion1", "Conditional Answer 1" }, 
+                { "conditionalQuestion2", "Conditional Answer 2" }
+            };
 
+            // Act
             List<IElement> listOfElements = elements.RemoveUnusedConditionalElements(viewModel);
 
+            // Assert
             Assert.Equal(3, listOfElements.Count);
             Assert.Equal(3, viewModel.Count);
         }
@@ -101,6 +121,7 @@ namespace form_builder_tests.UnitTests.Extensions
         [Fact]
         public void RemoveUnusedConditionalElements_ViewModelShould_HaveOneLessItem_WhenOnlySingleCheckboxValue_IsTicked()
         {
+            // Arrange
             List<IElement> elements = new List<IElement>();
             var conditionalElement1 = new ElementBuilder()
                 .WithType(EElementType.Textbox)
@@ -125,10 +146,17 @@ namespace form_builder_tests.UnitTests.Extensions
             elements.Add(conditionalElement1);
             elements.Add(conditionalElement2);
             elements.Add(element1);
-            var viewModel = new Dictionary<string, dynamic> { { "testQuestion", "Value1" }, { "conditionalQuestion1", "Conditional Answer 1" }, { "conditionalQuestion2", "Conditional Answer 2" } };
+            var viewModel = new Dictionary<string, dynamic>
+            {
+                { "testQuestion", "Value1" }, 
+                { "conditionalQuestion1", "Conditional Answer 1" }, 
+                { "conditionalQuestion2", "Conditional Answer 2" }
+            };
 
+            // Act
             List<IElement> listOfElements = elements.RemoveUnusedConditionalElements(viewModel);
 
+            // Assert
             Assert.Equal(2, listOfElements.Count);
             Assert.Equal(2, viewModel.Count);
         }
@@ -136,6 +164,7 @@ namespace form_builder_tests.UnitTests.Extensions
         [Fact]
         public void RemoveUnusedConditionalElements_ViewModelShould_Contain_SingleItem_WhenNoOptions_AreTicked_ForCheckbox()
         {
+            // Arrange
             List<IElement> elements = new List<IElement>();
             var conditionalElement1 = new ElementBuilder()
                 .WithType(EElementType.Textbox)
@@ -160,17 +189,25 @@ namespace form_builder_tests.UnitTests.Extensions
             elements.Add(conditionalElement1);
             elements.Add(conditionalElement2);
             elements.Add(element1);
-            var viewModel = new Dictionary<string, dynamic> { { "testQuestion", "" }, { "conditionalQuestion1", "Conditional Answer 1" }, { "conditionalQuestion2", "Conditional Answer 2" } };
+            var viewModel = new Dictionary<string, dynamic>
+            {
+                { "testQuestion", "" }, 
+                { "conditionalQuestion1", "Conditional Answer 1" }, 
+                { "conditionalQuestion2", "Conditional Answer 2" }
+            };
 
+            // Act
             List<IElement> listOfElements = elements.RemoveUnusedConditionalElements(viewModel);
 
+            // Assert
             Assert.Single(listOfElements);
             Assert.Single(viewModel);
         }
 
         [Fact]
-        public void RemoveUnusedConditionalElements_ElementListShould_Contain_OnlyConditionalElementsThatHaveCorrespondingOptions()
+        public void RemoveUnusedConditionalElements_ElementListShould_Contain_OnlyConditionalElementsThatHaveCorrespondingSelectedOptions()
         {
+            // Arrange
             List<IElement> elements = new List<IElement>();
             var conditionalElement1 = new ElementBuilder()
                 .WithType(EElementType.Textbox)
@@ -201,13 +238,81 @@ namespace form_builder_tests.UnitTests.Extensions
             elements.Add(conditionalElement2);
             elements.Add(conditionalElement3);
             elements.Add(element1);
-            var viewModel = new Dictionary<string, dynamic> { { "testQuestion", "Value1, Value2" }, { "conditionalQuestion1", "Conditional Answer 1" }, { "conditionalQuestion2", "Conditional Answer 2" } };
+            var viewModel = new Dictionary<string, dynamic>
+            {
+                { "testQuestion", "Value1, Value2" }, 
+                { "conditionalQuestion1", "Conditional Answer 1" }, 
+                { "conditionalQuestion2", "Conditional Answer 2" }
+            };
 
+            // Act
             List<IElement> listOfElements = elements.RemoveUnusedConditionalElements(viewModel);
 
+            // Assert
             Assert.Equal(3, listOfElements.Count);
             Assert.Equal(2, listOfElements.Count(_ => _.Properties.isConditionalElement));
             Assert.Equal(3, viewModel.Count);
+        }
+
+        [Fact]
+        public void RemoveUnusedConditionalElements_ElementListShould_Contain_OnlyConditionalElementsThatHaveCorrespondingSelectedOptions_MultipleElementsWithConditional()
+        {
+            // Arrange
+            List<IElement> elements = new List<IElement>();
+            var conditionalElement1 = new ElementBuilder()
+                .WithType(EElementType.Textbox)
+                .WithQuestionId("question1Conditional")
+                .WithValue("Conditional Answer 1")
+                .WithConditionalElement(true)
+                .Build();
+            var conditionalElement2 = new ElementBuilder()
+                .WithType(EElementType.Textbox)
+                .WithQuestionId("question2Conditional")
+                .WithValue("Conditional Answer 2")
+                .WithConditionalElement(true)
+                .Build();
+            var conditionalElement3 = new ElementBuilder()
+                .WithType(EElementType.Textbox)
+                .WithQuestionId("question2Conditional2")
+                .WithConditionalElement(true)
+                .Build();
+            var q1option1 = new Option { ConditionalElementId = "question1Conditional", Value = "Value1" };
+            var q1option2 = new Option { Value = "Value2" };
+            var q2option1 = new Option { ConditionalElementId = "question2Conditional", Value = "Value1" };
+            var q2option2 = new Option { ConditionalElementId = "question2Conditional2", Value = "Value2" };
+            var q1ListOfOptions = new List<Option> { q1option1, q1option2 };
+            var q2ListOfOptions = new List<Option> { q2option1, q2option2 };
+            var element1 = new ElementBuilder()
+                .WithType(EElementType.Radio)
+                .WithQuestionId("question1")
+                .WithOptions(q1ListOfOptions)
+                .Build();
+            var element2 = new ElementBuilder()
+                .WithType(EElementType.Radio)
+                .WithQuestionId("question2")
+                .WithOptions(q2ListOfOptions)
+                .Build();
+            elements.Add(conditionalElement1);
+            elements.Add(conditionalElement2);
+            elements.Add(conditionalElement3);
+            elements.Add(element1);
+            elements.Add(element2);
+            var viewModel = new Dictionary<string, dynamic>
+            {
+                { "question1", "Value1" }, 
+                { "question2", "Value1" }, 
+                { "question1Conditional", "Conditional Answer 1" }, 
+                { "question2Conditional", "Conditional Answer 2" }, 
+                { "question2Conditional2", "Conditional Answer 3" }
+            };
+
+            // Act
+            List<IElement> listOfElements = elements.RemoveUnusedConditionalElements(viewModel);
+
+            // Assert
+            Assert.Equal(4, listOfElements.Count);
+            Assert.Equal(2, listOfElements.Count(_ => _.Properties.isConditionalElement));
+            Assert.Equal(4, viewModel.Count);
         }
     }
 }
