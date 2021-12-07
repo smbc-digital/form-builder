c5ef66efa083b8f2d22c0a0aae247d8aa62dbdc6
stephen.lally <stephen.lally.smbc@gmail.com>
2021-12-07 11:01:56 +0000

    style: remove empty lines

--- src/Constants/SystemConstants.cs
+++ src/Constants/SystemConstants.cs
@@ -6,9 +6,7 @@ namespace form_builder.Constants
     public class SystemConstants
     {
         public static readonly List<string> AcceptedMimeTypes = new() { ".png", ".jpg", ".jpeg", ".pdf", ".docx", ".doc", ".odt" };
-
         public static readonly int DefaultMaxFileSize = 10485760;
-
         public static readonly int DefaultMaxCombinedFileSize = 24117248;
         public static readonly int OneMBInBinaryBytes = 1048576;
         public static readonly string CaseReferenceQueryString = "?caseReference=";
