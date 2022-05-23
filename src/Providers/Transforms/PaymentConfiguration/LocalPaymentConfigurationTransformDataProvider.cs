3e4389b3701df9e596d00306fad9803beca3c00e
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2022-05-23 14:34:03 +0100

    minor: amend local payment provider so that it can be used in all environments

--- src/Providers/Transforms/PaymentConfiguration/LocalPaymentConfigurationTransformDataProvider.cs
+++ src/Providers/Transforms/PaymentConfiguration/LocalPaymentConfigurationTransformDataProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using form_builder.Configuration;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 
@@ -11,9 +12,12 @@ namespace form_builder.Providers.Transforms.PaymentConfiguration
         private readonly string _fileBaseFolder = $@".\DSL\payment-config";
         private readonly LocalFileConfiguration _localFileConfig;
 
-        public LocalPaymentConfigurationTransformDataProvider(IOptions<LocalFileConfiguration> localFileConfig)
+        private readonly IWebHostEnvironment _environment;
+
+        public LocalPaymentConfigurationTransformDataProvider(IOptions<LocalFileConfiguration> localFileConfig, IWebHostEnvironment environment)
         {
             _localFileConfig = localFileConfig.Value;
+            _environment = environment;
             if(!string.IsNullOrEmpty(_localFileConfig.LocalPaymentConfigurationTransformBase))
                 _fileBaseFolder = _localFileConfig.LocalPaymentConfigurationTransformBase;
         }
@@ -21,7 +25,7 @@ namespace form_builder.Providers.Transforms.PaymentConfiguration
         public async Task<T> Get<T>()
         {
             return JsonConvert.DeserializeObject<T>(
-                await System.IO.File.ReadAllTextAsync($@"{_fileBaseFolder}\paymentconfiguration.local.json"));
+                await System.IO.File.ReadAllTextAsync($@"{_fileBaseFolder}\paymentconfiguration.{_environment.EnvironmentName}.json"));
         }
     }
 }
