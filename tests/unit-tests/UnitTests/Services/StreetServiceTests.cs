7b314956baef392121f3fd08c61354e5f2fb1b5e
Lydia Gray <lydia.gray@stockport.gov.uk>
2022-06-27 09:54:40 +0100

    fix(StreetServiceTests): fix spelling errors

--- tests/unit-tests/UnitTests/Services/StreetServiceTests.cs
+++ tests/unit-tests/UnitTests/Services/StreetServiceTests.cs
@@ -299,7 +299,7 @@ namespace form_builder_tests.UnitTests.Services
             var result = await Assert.ThrowsAsync<ApplicationException>(() => _service.ProcessStreet(viewModel, page, schema, "", "page-one"));
             _streetProvider.Verify(_ => _.SearchAsync(It.IsAny<string>()), Times.Once);
             _pageHelper.Verify(_ => _.GenerateHtml(It.IsAny<Page>(), It.IsAny<Dictionary<string, dynamic>>(), It.IsAny<FormSchema>(), It.IsAny<string>(), It.IsAny<FormAnswers>(), It.IsAny<List<object>>()), Times.Never);
-            Assert.StartsWith($"StreetService::ProccessInitialStreet: An exception has occured while attempting to perform street lookup on Provider '{fakeStreetProvider}' with searchterm 'streetname' Exception:", result.Message);
+            Assert.StartsWith($"StreetService::ProcessInitialStreet: An exception has occurred while attempting to perform street lookup on Provider '{fakeStreetProvider}' with searchterm 'streetname' Exception:", result.Message);
         }
     }
 }
