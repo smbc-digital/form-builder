9e6a4e718a6346fcf7140ac0edd1463da57819cf
Lydia Gray <lydia.gray@stockport.gov.uk>
2023-02-24 13:29:58 +0000

    refactor(EmailConfigurationProviders): refactor to handle local and s3 email configurations

--- src/Providers/Transforms/EmailConfiguration/LocalEmailConfigurationTransformDataProvider.cs
+++ src/Providers/Transforms/EmailConfiguration/LocalEmailConfigurationTransformDataProvider.cs
@@ -2,12 +2,12 @@
 
 namespace form_builder.Providers.Transforms.EmailConfiguration
 {
-    public class EmailConfigurationTransformDataProvider : IEmailConfigurationTransformDataProvider
+    public class LocalEmailConfigurationTransformDataProvider : IEmailConfigurationTransformDataProvider
     {
         public async Task<T> Get<T>()
         {
             return JsonConvert.DeserializeObject<T>(
-                await System.IO.File.ReadAllTextAsync($@".\DSL\email-config\emailconfiguration.json"));
+                await File.ReadAllTextAsync($@".\DSL\email-config\emailconfiguration.json"));
         }
     }
 }
