d344d138bae4b776d8949b4df286160e428759e0
Lydia Gray <34089008+lydiagray@users.noreply.github.com>
2024-04-23 10:56:48 +0100

    Develop (#910)
    * Submit and email logs (#907)
    
    * chore(emailsubmit): logging to a flow
    
    * chore(submitandemail): logging and tests
    
    * chore(submitandemail): comment suggestions
    
    * chore(submitandemail): comment suggestions
    
    * feat(KeyFormAccessRestrictions): add static token for QA access to internal forms (#909)
    
    ---------
    
    Co-authored-by: Chilejon <jon.chiles@stockport.gov.uk>

--- src/Restrictions/KeyFormAccessRestriction.cs
+++ src/Restrictions/KeyFormAccessRestriction.cs
@@ -1,12 +1,19 @@
+using form_builder.Configuration;
 using form_builder.Models;
+using Microsoft.Extensions.Options;
 
 namespace form_builder.Restrictions
 {
     public class KeyFormAccessRestriction : IFormAccessRestriction
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly QAFormAccessTokenConfiguration _qaFormAccessToken;
 
-        public KeyFormAccessRestriction(IHttpContextAccessor httpContextAccessor) => _httpContextAccessor = httpContextAccessor;
+        public KeyFormAccessRestriction(IHttpContextAccessor httpContextAccessor, IOptions<QAFormAccessTokenConfiguration> qaFormAccessToken)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _qaFormAccessToken = qaFormAccessToken.Value;
+        } 
 
         public bool IsRestricted(FormSchema baseForm)
         {
@@ -18,6 +25,9 @@ namespace form_builder.Restrictions
                 return true;
             
             var keyValuePair = context.Request.Query.Single(keyValuePair => keyValuePair.Key.Equals(baseForm.FormAccessKeyName));
+
+            if (keyValuePair.Value.Equals(_qaFormAccessToken.AccessKey))
+                return false;
 
             return !keyValuePair.Value.Equals(baseForm.FormAccessKey);
         }
