ad66f01a6c2df5a276a8d2509c8aeb50ac46290e
Jonathon Warwick <jonathon.warwick@stockport.gov.uk>
2024-07-15 14:34:49 +0100

    feat(properties): add in property for no street found text

--- src/Models/Properties/ElementProperties/StreetProperty.cs
+++ src/Models/Properties/ElementProperties/StreetProperty.cs
@@ -5,5 +5,7 @@
         public string StreetProvider { get; set; }
 
         public string StreetIAG { get; set; } = "The street must be in Stockport. You can only enter letters A to Z and spaces, for example, Stockport Road.";
+
+        public string StreetMissingText { get; set; }
     }
 }
