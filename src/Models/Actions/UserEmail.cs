e0759df39ce82da7f3931e7c349485f065b706ee
Jon Chiles <jonchiles@yahoo.com>
2024-07-16 08:10:19 +0100

    chore(UserEmail): basic option

--- src/Models/Actions/UserEmail.cs
+++ src/Models/Actions/UserEmail.cs
@@ -1,22 +1,41 @@
+using form_builder.ContentFactory.SuccessPageFactory;
 using form_builder.Enum;
 using form_builder.Helpers.ActionsHelpers;
 using form_builder.Providers.EmailProvider;
+using form_builder.Services.MappingService;
+using Newtonsoft.Json;
 
 namespace form_builder.Models.Actions
 {
     public class UserEmail : Action
     {
-        public UserEmail() => Type = EActionType.UserEmail;
+        private readonly ILogger<UserEmail> _logger;
+        public UserEmail(
+            ILogger<UserEmail> logger
+            )
+        {
+            Type = EActionType.UserEmail;
+            _logger = logger;
+        }
 
         public override async Task Process(IActionHelper actionHelper, IEmailProvider emailProvider, FormAnswers formAnswers)
         {
-            await emailProvider
-                .SendEmail(
-                    new EmailMessage(
-                        Properties.Subject,
-                        actionHelper.GetEmailContent(this, formAnswers),
-                        Properties.From,
-                        actionHelper.GetEmailToAddresses(this, formAnswers)));
+            try
+            {
+                await emailProvider
+                    .SendEmail(
+                        new EmailMessage(
+                            Properties.Subject,
+                            actionHelper.GetEmailContent(this, formAnswers),
+                            Properties.From,
+                            actionHelper.GetEmailToAddresses(this, formAnswers)));
+
+            }
+            catch (Exception)
+            {
+                //_logger.LogInformation($"{nameof(MappingService)}::{nameof(UserEmail)}: - Failed Email Send action - {JsonConvert.SerializeObject(formAnswers)}");
+                _logger.LogInformation($"UserEmail: - Failed Email Send action ");
+            }
         }
     }
 }
