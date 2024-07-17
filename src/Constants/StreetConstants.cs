4ce5a3c62ec6346f521bfcebc5dfc1ac8354e353
JonathonWarwick <144804141+JonathonWarwick@users.noreply.github.com>
2024-07-17 09:15:23 +0100

    Street hint (#923)
    * Develop (#919)
    
    * Submit and email logs (#907)
    
    * chore(emailsubmit): logging to a flow
    
    * chore(submitandemail): logging and tests
    
    * chore(submitandemail): comment suggestions
    
    * chore(submitandemail): comment suggestions
    
    * feat(KeyFormAccessRestrictions): add static token for QA access to internal forms (#909)
    
    * chore(logs): remove/change levels for a few logs (#911)
    
    * chore(SubmitService): log sent obj for missed bin collection only - TEMPORARY (#913)
    
    * fix(SubmitService): amend missed-bin-collection log data value (#915)
    
    * chore(Submit): more logs for missed collection missing data
    
    * chore(Logs): remove 2 logs, add new log to log raw cached session data (#918)
    
    ---------
    
    Co-authored-by: Chilejon <jon.chiles@stockport.gov.uk>
    
    * Develop (#921)
    
    * Submit and email logs (#907)
    
    * chore(emailsubmit): logging to a flow
    
    * chore(submitandemail): logging and tests
    
    * chore(submitandemail): comment suggestions
    
    * chore(submitandemail): comment suggestions
    
    * feat(KeyFormAccessRestrictions): add static token for QA access to internal forms (#909)
    
    * chore(logs): remove/change levels for a few logs (#911)
    
    * chore(SubmitService): log sent obj for missed bin collection only - TEMPORARY (#913)
    
    * fix(SubmitService): amend missed-bin-collection log data value (#915)
    
    * chore(Submit): more logs for missed collection missing data
    
    * chore(Logs): remove 2 logs, add new log to log raw cached session data (#918)
    
    * chore(Logs): remove logs, add logs around SaveAnswers for missed bin
    
    ---------
    
    Co-authored-by: Chilejon <jon.chiles@stockport.gov.uk>
    Co-authored-by: JonathonWarwick <144804141+JonathonWarwick@users.noreply.github.com>
    
    * chore(MappingService): delete a log thats being ignored by merge for some reason
    
    * chore(PageHelper): move log earlier (#922)
    
    * feat(properties): add in property for no street found text
    
    * fix(streetSelect): add in condition to only display hint if message populated
    
    ---------
    
    Co-authored-by: Lydia Gray <34089008+lydiagray@users.noreply.github.com>
    Co-authored-by: Chilejon <jon.chiles@stockport.gov.uk>
    Co-authored-by: Lydia Gray <lydia.gray@stockport.gov.uk>

--- src/Constants/StreetConstants.cs
+++ src/Constants/StreetConstants.cs
@@ -8,5 +8,6 @@ namespace form_builder.Constants
         public const string DESCRIPTION_SUFFIX = "-street-description";
         public static Regex STREET_REGEX = new Regex(@"^[a-zA-Z ]*$");
         public static int STREET_MIN_LENGTH = 3;
+        public const string STREET_MANUAL_TEXT = "I cannot find the street in the list";
     }
 }
