eafc5274b215658cd9b09929cc2e224fc5578e33
Jon Chiles <jonchiles@yahoo.com>
2024-10-14 11:35:35 +0100

    chore(osplace): filtering out the street from the results

--- src/Providers/Address/OSPlacesAddressProvider.cs
+++ src/Providers/Address/OSPlacesAddressProvider.cs
@@ -27,23 +27,21 @@ namespace form_builder.Providers.Address
 
         public async Task<IEnumerable<AddressSearchResult>> SearchAsync(string streetOrPostcode)
         {
-            var response = await _gateway.GetAsync($"{_oSPlacesAddressProviderConfiguration.Host}?query={streetOrPostcode}&fq=local_custodian_code:{_oSPlacesAddressProviderConfiguration.LocalCustodianCode}&key={_oSPlacesAddressProviderConfiguration.Key}&dataset=LPI");
+            var response = await _gateway.GetAsync($"{_oSPlacesAddressProviderConfiguration.Host}?query={streetOrPostcode}&fq=local_custodian_code:{_oSPlacesAddressProviderConfiguration.LocalCustodianCode}&fq=CLASSIFICATION_CODE:R*%20CLASSIFICATION_CODE:R*%20CLASSIFICATION_CODE:C*&key={_oSPlacesAddressProviderConfiguration.Key}&dataset=LPI");
             var result = await response.Content.ReadAsStringAsync();
             var addresses = JsonConvert.DeserializeObject<OSProperty>(result);
 
             try
             {
-                return addresses.results.Select(address => new AddressSearchResult
-                {
-                    AddressLine1 = address.LPI.PAO_START_NUMBER + " " + address.LPI.STREET_DESCRIPTION,
-                    AddressLine2 = address.LPI.LOCALITY_NAME,
-                    AddressLine3 = address.LPI.TOWN_NAME,
-                    Postcode = address.LPI.POSTCODE_LOCATOR,
-                    UniqueId = address.LPI.UPRN
-                }).OrderBy(_ => _.UniqueId);
-
-
-
+                return addresses.results
+                    .Select(address => new AddressSearchResult
+                    {
+                        AddressLine1 = address.LPI.PAO_START_NUMBER + " " + address.LPI.STREET_DESCRIPTION,
+                        AddressLine2 = address.LPI.LOCALITY_NAME,
+                        AddressLine3 = address.LPI.TOWN_NAME,
+                        Postcode = address.LPI.POSTCODE_LOCATOR,
+                        UniqueId = address.LPI.UPRN
+                    }).OrderBy(_ => _.UniqueId);
             }
             catch (Exception)
             {
