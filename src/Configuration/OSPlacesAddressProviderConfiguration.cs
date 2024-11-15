0390af6cab645bd165a00742c3ac3b89fa6381e1
Chilejon <jon.chiles@stockport.gov.uk>
2024-11-15 10:47:51 +0000

    chore(osplacesaddressprovider): getting a bearer token before making a call to api.os (#978)

--- src/Configuration/OSPlacesAddressProviderConfiguration.cs
+++ src/Configuration/OSPlacesAddressProviderConfiguration.cs
@@ -5,8 +5,8 @@ public class OSPlacesAddressProviderConfiguration
     public const string ConfigValue = "OSPlacesAddressProviderConfiguration";
 
     public string Key { get; set; }
-
     public string Host { get; set; }
-
     public string LocalCustodianCode { get; set; }
+    public string ClientID { get; set; }
+    public string ClientSecret { get; set; }
 }
