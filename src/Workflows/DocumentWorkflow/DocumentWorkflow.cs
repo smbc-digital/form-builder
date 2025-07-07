0d02724dedd5c928abe164bc34fb35619624bd56
Jon Chiles <jonchiles@yahoo.com>
2025-07-07 10:24:24 +0100

    chore(DocumentWorkflow): code clean up

--- src/Workflows/DocumentWorkflow/DocumentWorkflow.cs
+++ src/Workflows/DocumentWorkflow/DocumentWorkflow.cs
@@ -10,42 +10,42 @@ using Newtonsoft.Json;
 
 namespace form_builder.Workflows.DocumentWorkflow
 {
-    public class DocumentWorkflow : IDocumentWorkflow
-    {
-        private IDocumentSummaryService _documentSummaryService;
-        private readonly IDistributedCacheWrapper _distributedCache;
-        private readonly IPageHelper _pageHelper;
-        private readonly ISchemaFactory _schemaFactory;
+	public class DocumentWorkflow : IDocumentWorkflow
+	{
+		private IDocumentSummaryService _documentSummaryService;
+		private readonly IDistributedCacheWrapper _distributedCache;
+		private readonly IPageHelper _pageHelper;
+		private readonly ISchemaFactory _schemaFactory;
 
-        public DocumentWorkflow(IDocumentSummaryService documentSummaryService, IDistributedCacheWrapper distributedCache, ISchemaFactory schemaFactory, IPageHelper pageHelper)
-        {
-            _documentSummaryService = documentSummaryService;
-            _distributedCache = distributedCache;
-            _schemaFactory = schemaFactory;
-            _pageHelper = pageHelper;
-        }
+		public DocumentWorkflow(IDocumentSummaryService documentSummaryService, IDistributedCacheWrapper distributedCache, ISchemaFactory schemaFactory, IPageHelper pageHelper)
+		{
+			_documentSummaryService = documentSummaryService;
+			_distributedCache = distributedCache;
+			_schemaFactory = schemaFactory;
+			_pageHelper = pageHelper;
+		}
 
-        public async Task<byte[]> GenerateSummaryDocumentAsync(EDocumentType documentType, string id)
-        {
-            var previousAnswers = _pageHelper.GetSavedAnswers(id);
+		public async Task<byte[]> GenerateSummaryDocumentAsync(EDocumentType documentType, string id)
+		{
+			var previousAnswers = _pageHelper.GetSavedAnswers(id);
 
-            if (previousAnswers.FormName is null)
-            {
-                var formData = _distributedCache.GetString($"document-{id}");
+			if (previousAnswers.FormName is null)
+			{
+				var formData = _distributedCache.GetString($"document-{id}");
 
-                if (formData is null)
-                    throw new DocumentExpiredException($"DocumentWorkflow::GenerateSummaryDocument, Previous answers has expired, unable to generate {documentType.ToString()} document for summary");
+				if (formData is null)
+					throw new DocumentExpiredException($"DocumentWorkflow::GenerateSummaryDocument, Previous answers has expired, unable to generate {documentType.ToString()} document for summary");
 
-                previousAnswers = JsonConvert.DeserializeObject<FormAnswers>(formData);
-            }
-            var baseForm = await _schemaFactory.Build(previousAnswers.FormName);
+				previousAnswers = JsonConvert.DeserializeObject<FormAnswers>(formData);
+			}
+			var baseForm = await _schemaFactory.Build(previousAnswers.FormName);
 
-            return await _documentSummaryService.GenerateDocument(new DocumentSummaryEntity
-            {
-                DocumentType = documentType,
-                PreviousAnswers = previousAnswers,
-                FormSchema = baseForm
-            });
-        }
-    }
+			return await _documentSummaryService.GenerateDocument(new DocumentSummaryEntity
+			{
+				DocumentType = documentType,
+				PreviousAnswers = previousAnswers,
+				FormSchema = baseForm
+			});
+		}
+	}
 }
