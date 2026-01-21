f1d1decf42fc2b5cdc4c4e117f589d876dbbcd65
Lydia Gray <34089008+lydiagray@users.noreply.github.com>
2026-01-21 13:25:54 +0000

    feat(StrongTagParser): TagParser for <strong> html (#1068)

---
+++ src/TagParsers/StrongTagParser.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.RegularExpressions;
+using form_builder.Models;
+using form_builder.TagParsers.Formatters;
+
+namespace form_builder.TagParsers;
+
+public class StrongTagParser : TagParser, ITagParser
+{
+    public StrongTagParser(IEnumerable<IFormatter> formatters) : base(formatters) { }
+
+    public Regex Regex => new Regex("(?<={{)STRONG:.*?(?=}})", RegexOptions.Compiled);
+
+    public async Task<Page> Parse(Page page, FormAnswers formAnswers, FormSchema baseForm = null)
+    {
+        page.Elements.Select((element) =>
+        {
+            if (!string.IsNullOrEmpty(element.Properties?.Text))
+                element.Properties.Text = Parse(element.Properties.Text, Regex);
+
+            if (!string.IsNullOrEmpty(element.Properties?.Hint))
+                element.Properties.Hint = Parse(element.Properties.Hint, Regex);
+
+            return element;
+        }).ToList();
+
+        return await Task.FromResult(page);
+    }
+
+    public string ParseString(string content, FormAnswers formAnswers)
+    {
+        var updatedContent = content;
+        var matches = Regex.Matches(content);
+
+        foreach (Match match in matches)
+        {
+            var value = $"{{{{{match.Value}}}}}";
+
+            try
+            {
+                var replacedContent = Parse(value, Regex);
+                updatedContent = updatedContent.Replace(value, replacedContent);
+            }
+            catch (Exception)
+            {
+                updatedContent = updatedContent.Replace(value, string.Empty);
+            }
+        }
+
+        return updatedContent;
+    }
+}
---
+++ tests/unit-tests/UnitTests/TagParsers/StrongTagParserTests.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using form_builder.Builders;
+using form_builder.Enum;
+using form_builder.Models;
+using form_builder.TagParsers;
+using form_builder.TagParsers.Formatters;
+using Moq;
+using Xunit;
+
+namespace form_builder_tests.UnitTests.TagParsers;
+
+public class StrongTagParserTests
+{
+    private readonly Mock<IEnumerable<IFormatter>> _mockFormatters = new();
+    private readonly StrongTagParser _tagParser;
+
+    public StrongTagParserTests()
+    {
+        _tagParser = new StrongTagParser(_mockFormatters.Object);
+    }
+
+    [Theory]
+    [InlineData("{{STRONG:text}}")]
+    [InlineData("{{STRONG:text of multiple words}}")]
+    public void Regex_ShouldReturnTrue_Result(string value)
+    {
+        Assert.True(_tagParser.Regex.Match(value).Success);
+    }
+
+    [Theory]
+    [InlineData("{{STRING:text}}")]
+    [InlineData("{{STRONGG:text}}")]
+    [InlineData("{{strong:text}}")]
+    [InlineData("{STRONG:text}")]
+    [InlineData("{{TAG:text}}")]
+    public void Regex_ShouldReturnFalse_Result(string value)
+    {
+        Assert.False(_tagParser.Regex.Match(value).Success);
+    }
+
+    [Fact]
+    public async Task Parse_ShouldReturnInitialValue_WhenNoValuesAre_To_BeReplaced()
+    {
+        var element = new ElementBuilder()
+            .WithType(EElementType.P)
+            .WithPropertyText("this has no values to be replaced")
+            .Build();
+
+        var page = new PageBuilder()
+            .WithElement(element)
+            .Build();
+
+        var formAnswers = new FormAnswers();
+
+        var result = await _tagParser.Parse(page, formAnswers);
+
+        Assert.Equal(element.Properties.Text, result.Elements.FirstOrDefault().Properties.Text);
+    }
+
+    [Fact]
+    public async Task Parse_ShouldReturnInitialValue_When_NoTag_MatchesRegex()
+    {
+        var element = new ElementBuilder()
+            .WithType(EElementType.P)
+            .WithPropertyText("this value {{TAG:firstname}} should not be replaced as it is invalid")
+            .Build();
+
+        var page = new PageBuilder()
+            .WithElement(element)
+            .Build();
+
+        var formAnswers = new FormAnswers();
+
+        var result = await _tagParser.Parse(page, formAnswers);
+
+        Assert.Equal(element.Properties.Text, result.Elements.FirstOrDefault().Properties.Text);
+    }
+
+    [Fact]
+    public async Task Parse_ShouldReturnUpdatedValue_WhenReplacingSingleValue()
+    {
+        var expectedString = "this <strong>text</strong> should have strong tags";
+
+        var element = new ElementBuilder()
+           .WithType(EElementType.P)
+           .WithPropertyText("this {{STRONG:text}} should have strong tags")
+           .Build();
+
+        var page = new PageBuilder()
+            .WithElement(element)
+            .Build();
+
+        var formAnswers = new FormAnswers();
+
+        var result = await _tagParser.Parse(page, formAnswers);
+        Assert.Equal(expectedString, result.Elements.FirstOrDefault().Properties.Text);
+    }
+
+    [Fact]
+    public async Task Parse_ShouldReturnUpdatedValueForHint_WhenReplacingSingleValue()
+    {
+        var expectedString = "this <strong>text</strong> should have strong tags";
+
+        var element = new ElementBuilder()
+            .WithType(EElementType.Textbox)
+            .WithHint("this {{STRONG:text}} should have strong tags")
+            .Build();
+
+        var page = new PageBuilder()
+            .WithElement(element)
+            .Build();
+
+        var formAnswers = new FormAnswers();
+
+        var result = await _tagParser.Parse(page, formAnswers);
+        Assert.Equal(expectedString, result.Elements.FirstOrDefault().Properties.Hint);
+    }
+
+    [Fact]
+    public async Task Parse_ShouldReturnUpdatedValue_WhenReplacingMultipleValues()
+    {
+        var expectedString = "this <strong>text</strong> should have strong tags, and so should <strong>this text</strong>";
+
+        var element = new ElementBuilder()
+            .WithType(EElementType.P)
+            .WithPropertyText("this {{STRONG:text}} should have strong tags, and so should {{STRONG:this text}}")
+            .Build();
+
+        var page = new PageBuilder()
+            .WithElement(element)
+            .Build();
+
+        var formAnswers = new FormAnswers();
+
+        var result = await _tagParser.Parse(page, formAnswers);
+        Assert.Equal(expectedString, result.Elements.FirstOrDefault().Properties.Text);
+    }
+
+    [Fact]
+    public void ParseString_ShouldReturnInitialValue_WhenNoValuesAre_To_BeReplaced()
+    {
+        var text = "this has no values to be replaced";
+
+        var formAnswers = new FormAnswers();
+
+        var result = _tagParser.ParseString(text, formAnswers);
+
+        Assert.Equal(text, result);
+    }
+
+    [Fact]
+    public void ParseString_ShouldReturnInitialValue_When_NoTag_MatchesRegex()
+    {
+        var text = "this value {{TAG:firstname}} should not be replaced as it is invalid";
+
+        var formAnswers = new FormAnswers();
+
+        var result = _tagParser.ParseString(text, formAnswers);
+
+        Assert.Equal(text, result);
+    }
+
+    [Fact]
+    public void ParseString_ShouldReturnUpdatedValue_WhenReplacingSingleValue()
+    {
+        var expectedString = "this <strong>text</strong> should have strong tags";
+        var text = "this {{STRONG:text}} should have strong tags";
+
+        var formAnswers = new FormAnswers();
+
+        var result = _tagParser.ParseString(text, formAnswers);
+
+        Assert.Equal(expectedString, result);
+    }
+
+    [Fact]
+    public void ParseString_ShouldReturnUpdatedValue_WhenReplacingMultipleValues()
+    {
+        var expectedString = "this <strong>text</strong> should have strong tags, and so should <strong>this text</strong>";
+        var text = "this {{STRONG:text}} should have strong tags, and so should {{STRONG:this text}}";
+
+        var formAnswers = new FormAnswers();
+
+        var result = _tagParser.ParseString(text, formAnswers);
+
+        Assert.Equal(expectedString, result);
+    }
+}
