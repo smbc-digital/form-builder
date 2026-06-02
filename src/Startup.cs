946a96594532eb2379e0e62affb2a7a2ce8e52b9
Lydia Gray <lydia.gray@stockport.gov.uk>
2026-06-02 12:57:31 +0100

    chore(StartUp): test CookieSecurePolicy.Always

--- src/Startup.cs
+++ src/Startup.cs
@@ -70,7 +70,7 @@ namespace form_builder
                 .AddAntiforgery(_ =>
                     {
                         _.Cookie.Name = ".formbuilder.antiforgery.v2";
-                        _.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+                        _.Cookie.SecurePolicy = CookieSecurePolicy.Always;
                         _.Cookie.SameSite = SameSiteMode.None;
                     })
                 .AddSession(_ =>
@@ -78,7 +78,7 @@ namespace form_builder
                     _.IdleTimeout = TimeSpan.FromMinutes(60);
                     _.Cookie.Path = "/";
                     _.Cookie.Name = ".formbuilder.v2";
-                    _.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+                    _.Cookie.SecurePolicy = CookieSecurePolicy.Always;
                     _.Cookie.SameSite = SameSiteMode.None;
                 });
 
